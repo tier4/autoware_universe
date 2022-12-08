paulsohn/autoware_universe_planning_2

fix(behavior_path_planner): fix lane change path validation (#2421)
* fix(behavior_path_planner): clip_path_for_goal

Signed-off-by: Fumiya Watanabe <rej55.g@gmail.com>

* fix(behavior_path_planner): clip path for validation

Signed-off-by: Fumiya Watanabe <rej55.g@gmail.com>

* fix(behavior_path_planner): use path point only to check lane departure

Signed-off-by: Fumiya Watanabe <rej55.g@gmail.com>

* fix(behavior_path_planner): remove unnecessary change

Signed-off-by: Fumiya Watanabe <rej55.g@gmail.com>

Signed-off-by: Fumiya Watanabe <rej55.g@gmail.com>

planning/behavior_path_planner/src/scene_module/lane_change/util.cpp
@@ -496,7 +496,12 @@ PathWithLaneId getReferencePathFromTargetLane(
   const ArcCoordinates lane_change_start_arc_position =
     lanelet::utils::getArcCoordinates(target_lanes, lane_changing_start_pose);
   const double & s_start = lane_change_start_arc_position.length;
-  const double s_end = s_start + prepare_distance + lane_changing_distance + forward_path_length;
+  double s_end = s_start + prepare_distance + lane_changing_distance + forward_path_length;
+  if (route_handler.isInGoalRouteSection(target_lanes.back())) {
+    const auto goal_arc_coordinates =
+      lanelet::utils::getArcCoordinates(target_lanes, route_handler.getGoalPose());
+    s_end = std::min(s_end, goal_arc_coordinates.length);
+  }
   return route_handler.getCenterLinePath(target_lanes, s_start, s_end);
 }
 
@@ -511,7 +516,12 @@ PathWithLaneId getReferencePathFromTargetLane(
     lanelet::utils::getArcCoordinates(target_lanes, in_target_end_pose);
 
   const double & s_start = lane_change_start_arc_position.length;
-  const double & s_end = lane_change_end_arc_position.length;
+  double s_end = lane_change_end_arc_position.length;
+  if (route_handler.isInGoalRouteSection(target_lanes.back())) {
+    const auto goal_arc_coordinates =
+      lanelet::utils::getArcCoordinates(target_lanes, route_handler.getGoalPose());
+    s_end = std::min(s_end, goal_arc_coordinates.length);
+  }
   return route_handler.getCenterLinePath(target_lanes, s_start, s_end);
 }
 

tier4/autoware_universe

Signed-off-by: tomoya.kimura <tomoya.kimura@tier4.jp>

planning/behavior_path_planner/src/scene_module/lane_change/util.cpp
@@ -172,6 +172,11 @@ std::vector<LaneChangePath> getLaneChangePaths(
           target_lanelets, reference_path1.points.back().point.pose);
       double s_start = lane_change_start_arc_position.length;
       double s_end = s_start + straight_distance + lane_change_distance + forward_path_length;
+      if (route_handler.isInGoalRouteSection(target_lanelets.back())) {
+        const auto goal_arc_coordinates =
+          lanelet::utils::getArcCoordinates(target_lanelets, route_handler.getGoalPose());
+        s_end = std::min(s_end, goal_arc_coordinates.length);
+      }
       target_lane_reference_path = route_handler.getCenterLinePath(target_lanelets, s_start, s_end);
     }
 