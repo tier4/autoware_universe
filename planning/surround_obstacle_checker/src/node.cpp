1f5dca54a44ebc4beffc28b35c0d0807b2f26de0
Hiroki OTA <hiroki.ota@tier4.jp>
2022-12-23 16:35:42 +0900

    Update node.cpp

--- planning/surround_obstacle_checker/src/node.cpp
+++ planning/surround_obstacle_checker/src/node.cpp
@@ -394,7 +394,7 @@ Polygon2d SurroundObstacleCheckerNode::createSelfPolygon()
   poly.outer().push_back(Point2d(front, right));
   poly.outer().push_back(Point2d(rear, right));
   poly.outer().push_back(Point2d(rear, left));
-  bg::correct(poly);
+  boost::geometry::correct(poly);
   return poly;
 }
 
