cb8d8a9ee75c43283547586fce6bf33f9834d10d
Mamoru Sobue <mamoru.sobue@tier4.jp>
2023-04-28 16:52:03 +0900

    fix(intersection): always send finite distance to occlusion peeking stop line in RTC
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- planning/behavior_velocity_planner/src/scene_module/intersection/scene_intersection.cpp
+++ planning/behavior_velocity_planner/src/scene_module/intersection/scene_intersection.cpp
@@ -368,24 +368,18 @@ bool IntersectionModule::modifyPathVelocity(PathWithLaneId * path, StopReason * 
                                                      : StateMachine::State::GO,
     logger_.get_child("collision state_machine"), *clock_);
 
-  /* set RTC request respectively */
+  /* set RTC safety respectively */
+  occlusion_first_stop_distance_ = dist_1st_stopline;
+  occlusion_stop_distance_ = dist_2nd_stopline;
+  setDistance(dist_1st_stopline);
   if (occlusion_stop_required) {
     if (first_phase_stop_required) {
       occlusion_first_stop_safety_ = false;
-      occlusion_first_stop_distance_ = motion_utils::calcSignedArcLength(
-        path->points, planner_data_->current_odometry->pose.position,
-        path->points.at(stop_line_idx.value()).point.pose.position);
     }
-    occlusion_safety_ = false;
-    occlusion_stop_distance_ = motion_utils::calcSignedArcLength(
-      path->points, planner_data_->current_odometry->pose.position,
-      path->points.at(occlusion_peeking_line_idx.value()).point.pose.position);
+    occlusion_safety_ = is_occlusion_cleared;
   } else {
     /* collision */
     setSafe(collision_state_machine_.getState() == StateMachine::State::GO);
-    setDistance(motion_utils::calcSignedArcLength(
-      path->points, planner_data_->current_odometry->pose.position,
-      path->points.at(stop_line_idx.value()).point.pose.position));
   }
 
   /* make decision */
