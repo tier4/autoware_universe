d59aa49aec4713594603874d100faff76a6301ba
Satoshi OTA <44889564+satoshi-ota@users.noreply.github.com>
2023-05-11 22:22:44 +0900

    fix(behavior_path_planner): fix isActivated behavior (#3663)
    * fix(behavior_path_planner): return FALSE when the uuid is not found
    
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>
    
    * fix(behavior_path_planner): fix isActivated behavior
    
    Signed-off-by: Fumiya Watanabe <rej55.g@gmail.com>
    
    * keep rtc_interface_ptr_map_ empty for lane_following and side_shift
    
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
    
    ---------
    
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>
    Signed-off-by: Fumiya Watanabe <rej55.g@gmail.com>
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
    Co-authored-by: Fumiya Watanabe <rej55.g@gmail.com>
    Co-authored-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>

--- planning/behavior_path_planner/include/behavior_path_planner/scene_module/scene_module_interface.hpp
+++ planning/behavior_path_planner/include/behavior_path_planner/scene_module/scene_module_interface.hpp
@@ -234,6 +234,10 @@ public:
    */
   bool isActivated()
   {
+    if (rtc_interface_ptr_map_.empty()) {
+      return true;
+    }
+
     for (auto itr = rtc_interface_ptr_map_.begin(); itr != rtc_interface_ptr_map_.end(); ++itr) {
       if (itr->second->isRegistered(uuid_map_.at(itr->first))) {
         return itr->second->isActivated(uuid_map_.at(itr->first));
