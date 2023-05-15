940839560dd1458b8f57ce6ee50f73bea22ebd1e
h-ohta <hiroki.ota@tier4.jp>
2023-05-15 12:38:35 +0900

    fix(tier4_perception_rviz_plugin): fix build error in Humble

--- common/tier4_perception_rviz_plugin/src/tools/interactive_object.hpp
+++ common/tier4_perception_rviz_plugin/src/tools/interactive_object.hpp
@@ -66,7 +66,7 @@
 
 #include <boost/optional.hpp>
 
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/transform_listener.h>
 
 #include <algorithm>
