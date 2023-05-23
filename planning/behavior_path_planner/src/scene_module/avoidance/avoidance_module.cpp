65a50655b543423285764ae59915b64ee745d065
Satoshi OTA <44889564+satoshi-ota@users.noreply.github.com>
2023-05-23 22:17:17 +0900

    fix(avoidance): reset rtc status in yield status (#3798)
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

--- planning/behavior_path_planner/src/scene_module/avoidance/avoidance_module.cpp
+++ planning/behavior_path_planner/src/scene_module/avoidance/avoidance_module.cpp
@@ -593,7 +593,8 @@ void AvoidanceModule::updateEgoBehavior(const AvoidancePlanningData & data, Shif
     case AvoidanceState::YIELD: {
       insertYieldVelocity(path);
       insertWaitPoint(parameters_->use_constraints_for_decel, path);
-      removeRegisteredShiftLines();
+      initRTCStatus();
+      unlockNewModuleLaunch();
       break;
     }
     case AvoidanceState::AVOID_PATH_NOT_READY: {
