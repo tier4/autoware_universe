a5e52a7b4d4e5028a304df5d3fd23006b7a9c6d0
Tomoya Kimura <tomoya.kimura@tier4.jp>
2023-07-20 14:09:51 +0900

    fix(behavior_velocity_intersection_module): prevent invalid access for empty polygon (#671)
    Signed-off-by: tomoya.kimura <tomoya.kimura@tier4.jp>

--- planning/behavior_velocity_intersection_module/src/scene_intersection.cpp
+++ planning/behavior_velocity_intersection_module/src/scene_intersection.cpp
@@ -978,6 +978,10 @@ bool IntersectionModule::checkCollision(
         const auto trimmed_ego_polygon =
           getPolygonFromArcLength(ego_lane_with_next_lane, start_arc_length, end_arc_length);
 
+        if (trimmed_ego_polygon.empty()) {
+          continue;
+        }
+
         Polygon2d polygon{};
         for (const auto & p : trimmed_ego_polygon) {
           polygon.outer().emplace_back(p.x(), p.y());
