paulsohn/autoware_universe_planning_2

fix(behavior_path_planner): fix simultaneous execution (#4622)
Signed-off-by: kosuke55 <kosuke.tnp@gmail.com>

planning/behavior_path_planner/src/scene_module/start_planner/manager.cpp
@@ -105,12 +105,55 @@ void StartPlannerModuleManager::updateModuleParams(
       const auto start_planner_ptr = std::dynamic_pointer_cast<StartPlannerModule>(observer.lock());
       if (start_planner_ptr) {
         start_planner_ptr->updateModuleParams(p);
-        start_planner_ptr->setInitialIsSimultaneousExecutableAsApprovedModule(
-          enable_simultaneous_execution_as_approved_module_);
-        start_planner_ptr->setInitialIsSimultaneousExecutableAsCandidateModule(
-          enable_simultaneous_execution_as_candidate_module_);
       }
     }
   });
+}
+
+bool StartPlannerModuleManager::isSimultaneousExecutableAsApprovedModule() const
+{
+  if (observers_.empty()) {
+    return enable_simultaneous_execution_as_approved_module_;
+  }
+
+  const auto checker = [this](const SceneModuleObserver & observer) {
+    if (observer.expired()) {
+      return enable_simultaneous_execution_as_approved_module_;
+    }
+
+    const auto start_planner_ptr = std::dynamic_pointer_cast<StartPlannerModule>(observer.lock());
+
+    // Currently simultaneous execution with other modules is not supported while backward driving
+    if (!start_planner_ptr->isBackFinished()) {
+      return false;
+    }
+
+    return enable_simultaneous_execution_as_approved_module_;
+  };
+
+  return std::all_of(observers_.begin(), observers_.end(), checker);
+}
+
+bool StartPlannerModuleManager::isSimultaneousExecutableAsCandidateModule() const
+{
+  if (observers_.empty()) {
+    return enable_simultaneous_execution_as_candidate_module_;
+  }
+
+  const auto checker = [this](const SceneModuleObserver & observer) {
+    if (observer.expired()) {
+      return enable_simultaneous_execution_as_candidate_module_;
+    }
+
+    const auto start_planner_ptr = std::dynamic_pointer_cast<StartPlannerModule>(observer.lock());
+
+    // Currently simultaneous execution with other modules is not supported while backward driving
+    if (!start_planner_ptr->isBackFinished()) {
+      return false;
+    }
+    return enable_simultaneous_execution_as_candidate_module_;
+  };
+
+  return std::all_of(observers_.begin(), observers_.end(), checker);
 }
 }  // namespace behavior_path_planner

tier4/autoware_universe

planning/behavior_path_planner/src/scene_module/start_planner/manager.cpp
@@ -102,10 +102,32 @@ void StartPlannerModuleManager::updateModuleParams(
 
   std::for_each(registered_modules_.begin(), registered_modules_.end(), [&](const auto & m) {
     m->updateModuleParams(p);
-    m->setInitialIsSimultaneousExecutableAsApprovedModule(
-      enable_simultaneous_execution_as_approved_module_);
-    m->setInitialIsSimultaneousExecutableAsCandidateModule(
-      enable_simultaneous_execution_as_candidate_module_);
   });
+}
+
+bool StartPlannerModuleManager::isSimultaneousExecutableAsApprovedModule() const
+{
+  const auto checker = [this](const auto & module) {
+    // Currently simultaneous execution with other modules is not supported while backward driving
+    if (!module->isBackFinished()) {
+      return false;
+    }
+    return enable_simultaneous_execution_as_candidate_module_;
+  };
+
+  return std::all_of(registered_modules_.begin(), registered_modules_.end(), checker);
+}
+
+bool StartPlannerModuleManager::isSimultaneousExecutableAsCandidateModule() const
+{
+  const auto checker = [this](const auto & module) {
+    // Currently simultaneous execution with other modules is not supported while backward driving
+    if (!module->isBackFinished()) {
+      return false;
+    }
+    return enable_simultaneous_execution_as_candidate_module_;
+  };
+
+  return std::all_of(registered_modules_.begin(), registered_modules_.end(), checker);
 }
 }  // namespace behavior_path_planner