autowarefoundation/autoware.universe

fix(behavior_path_planner): prevent crashes with the drivable area expansion

planning/behavior_path_planner/src/utils/drivable_area_expansion/expansion.cpp
@@ -59,7 +59,7 @@ polygon_t createExpansionPolygon(
   boost::geometry::buffer(
     base_ls, polygons, distance_strategy, strategy::side_straight(), strategy::join_miter(),
     strategy::end_flat(), strategy::point_square());
-  return polygons.front();
+  return polygons.empty() ? polyton_t{} : polygons.front();
 }
 
 std::array<double, 3> calculate_arc_length_range_and_distance(

tier4/autoware_universe

planning/behavior_path_planner/src/utils/drivable_area_expansion/expansion.cpp
@@ -64,7 +64,7 @@ polygon_t createExpansionPolygon(
   boost::geometry::buffer(
     base_ls, polygons, distance_strategy, strategy::side_straight(), strategy::join_miter(),
     strategy::end_flat(), strategy::point_square());
-  return polygons.front();
+  return polygons.empty() ? polygon_t{} : polygons.front();
 }
 
 std::array<double, 3> calculate_arc_length_range_and_distance(