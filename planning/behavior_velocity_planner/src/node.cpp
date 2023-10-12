046c467447186a2f087900a0baa5ac45434f4c37
h-ohta <hiroki.ota@tier4.jp>
2023-10-12 20:07:12 +0900

    fix: not interpolate behavior velocity path

--- planning/behavior_velocity_planner/src/node.cpp
+++ planning/behavior_velocity_planner/src/node.cpp
@@ -420,10 +420,10 @@ void BehaviorVelocityPlannerNode::onTrigger(
   const auto filtered_path = filterLitterPathPoint(to_path(velocity_planned_path));
 
   // interpolation
-  const auto interpolated_path_msg = interpolatePath(filtered_path, forward_path_length_);
+  // const auto interpolated_path_msg = interpolatePath(filtered_path, forward_path_length_);
 
   // check stop point
-  auto output_path_msg = filterStopPathPoint(interpolated_path_msg);
+  auto output_path_msg = filterStopPathPoint(filtered_path);
   output_path_msg.header.frame_id = "map";
   output_path_msg.header.stamp = this->now();
 
