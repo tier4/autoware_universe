1f2d4728c76aee68879c814ea22aa610b6ac4144
keita1523 <keita.miura@tier4.jp>
2023-12-01 04:33:24 +0900

    feat: return empty data
    Signed-off-by: keita1523 <keita.miura@tier4.jp>

--- localization/ndt_scan_matcher/src/ndt_scan_matcher_core.cpp
+++ localization/ndt_scan_matcher/src/ndt_scan_matcher_core.cpp
@@ -313,6 +313,7 @@ void NDTScanMatcher::callback_sensor_points(
 {
   // mutex ndt_ptr_
   std::lock_guard<std::mutex> lock(ndt_ptr_mtx_);
+  if (sensor_points_sensorTF_msg_ptr->data.empty()) return;
 
   const auto exe_start_time = std::chrono::system_clock::now();
   const rclcpp::Time sensor_ros_time = sensor_points_sensorTF_msg_ptr->header.stamp;
