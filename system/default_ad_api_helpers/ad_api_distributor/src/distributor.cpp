6a23e6059d721c0307954eb70c1b58965bf945f0
veqcc <ryuta.kambe@tier4.jp>
2023-12-13 18:43:43 +0900

    implement distributor node
    Signed-off-by: veqcc <ryuta.kambe@tier4.jp>

--- system/default_ad_api_helpers/ad_api_distributor/src/distributor.cpp
+++ system/default_ad_api_helpers/ad_api_distributor/src/distributor.cpp
@@ -21,6 +21,82 @@ namespace ad_api_distributor
 
 Distributor::Distributor() : Node("distributor")
 {
+  using std::placeholders::_1;
+  using std::placeholders::_2;
+
+  // Service
+  srv_initialize_ = create_service<autoware_adapi_v1_msgs::srv::InitializeLocalization>(
+    "/api/localization/initialize", std::bind(&Distributor::on_initialize, this, _1, _2));
+  srv_set_route_ = create_service<autoware_adapi_v1_msgs::srv::SetRoute>(
+    "/api/routing/set_route", std::bind(&Distributor::on_set_route, this, _1, _2));
+  srv_clear_route_ = create_service<autoware_adapi_v1_msgs::srv::ClearRoute>(
+    "/api/routing/clear_route", std::bind(&Distributor::on_clear_route, this, _1, _2));
+  srv_stop_mode_ = create_service<autoware_adapi_v1_msgs::srv::ChangeOperationMode>(
+    "/api/operation_mode/change_to_stop", std::bind(&Distributor::on_change_to_stop, this, _1, _2));
+  srv_autonomous_mode_ = create_service<autoware_adapi_v1_msgs::srv::ChangeOperationMode>(
+    "/api/operation_mode/change_to_autonomous", std::bind(&Distributor::on_change_to_autonomous, this, _1, _2));
+
+  // Client
+  cli_main_initialize_ = create_client<autoware_adapi_v1_msgs::srv::InitializeLocalization>(
+    "/main/api/localization/initialize");
+  cli_sub_initialize_ = create_client<autoware_adapi_v1_msgs::srv::InitializeLocalization>(
+    "/sub/api/localization/initialize");
+  cli_main_set_route_ = create_client<autoware_adapi_v1_msgs::srv::SetRoute>(
+    "/main/api/localization/initialize");
+  cli_sub_set_route_ = create_client<autoware_adapi_v1_msgs::srv::SetRoute>(
+    "/sub/api/localization/initialize");
+  cli_main_clear_route_ = create_client<autoware_adapi_v1_msgs::srv::ClearRoute>(
+    "/main/api/localization/initialize");
+  cli_sub_clear_route_ = create_client<autoware_adapi_v1_msgs::srv::ClearRoute>(
+    "/sub/api/localization/initialize");
+  cli_main_stop_mode_ = create_client<autoware_adapi_v1_msgs::srv::ChangeOperationMode>(
+    "/main/api/localization/initialize");
+  cli_sub_stop_mode_ = create_client<autoware_adapi_v1_msgs::srv::ChangeOperationMode>(
+    "/sub/api/localization/initialize");
+  cli_main_autonomous_mode_ = create_client<autoware_adapi_v1_msgs::srv::ChangeOperationMode>(
+    "/main/api/localization/initialize");
+  cli_sub_autonomous_mode_ = create_client<autoware_adapi_v1_msgs::srv::ChangeOperationMode>(
+    "/sub/api/localization/initialize");
+}
+
+void Distributor::on_initialize(
+  const autoware_adapi_v1_msgs::srv::InitializeLocalization::Request::SharedPtr req,
+  const autoware_adapi_v1_msgs::srv::InitializeLocalization::Response::SharedPtr)
+{
+  cli_main_initialize_->async_send_request(req);
+  cli_sub_initialize_->async_send_request(req);
+}
+
+void Distributor::on_set_route(
+  const autoware_adapi_v1_msgs::srv::SetRoute::Request::SharedPtr req,
+  const autoware_adapi_v1_msgs::srv::SetRoute::Response::SharedPtr)
+{
+  cli_main_set_route_->async_send_request(req);
+  cli_sub_set_route_->async_send_request(req);
+}
+
+void Distributor::on_clear_route(
+  const autoware_adapi_v1_msgs::srv::ClearRoute::Request::SharedPtr req,
+  const autoware_adapi_v1_msgs::srv::ClearRoute::Response::SharedPtr)
+{
+  cli_main_clear_route_->async_send_request(req);
+  cli_sub_clear_route_->async_send_request(req);
+}
+
+void Distributor::on_change_to_stop(
+  const autoware_adapi_v1_msgs::srv::ChangeOperationMode::Request::SharedPtr req,
+  const autoware_adapi_v1_msgs::srv::ChangeOperationMode::Response::SharedPtr)
+{
+  cli_main_stop_mode_->async_send_request(req);
+  cli_sub_stop_mode_->async_send_request(req);
+}
+
+void Distributor::on_change_to_autonomous(
+  const autoware_adapi_v1_msgs::srv::ChangeOperationMode::Request::SharedPtr req,
+  const autoware_adapi_v1_msgs::srv::ChangeOperationMode::Response::SharedPtr)
+{
+  cli_main_autonomous_mode_->async_send_request(req);
+  cli_sub_autonomous_mode_->async_send_request(req);
 }
 
 }  // namespace ad_api_distributor
--- system/default_ad_api_helpers/ad_api_distributor/src/distributor.hpp
+++ system/default_ad_api_helpers/ad_api_distributor/src/distributor.hpp
@@ -15,6 +15,11 @@
 #ifndef DISTRIBUTOR_HPP_
 #define DISTRIBUTOR_HPP_
 
+#include <autoware_adapi_v1_msgs/srv/initialize_localization.hpp>
+#include <autoware_adapi_v1_msgs/srv/clear_route.hpp>
+#include <autoware_adapi_v1_msgs/srv/set_route.hpp>
+#include <autoware_adapi_v1_msgs/srv/change_operation_mode.hpp>
+
 #include <rclcpp/rclcpp.hpp>
 
 namespace ad_api_distributor
@@ -24,6 +29,47 @@ class Distributor : public rclcpp::Node
 {
 public:
   Distributor();
+
+private:
+  // Service
+  rclcpp::Service<autoware_adapi_v1_msgs::srv::InitializeLocalization>::SharedPtr srv_initialize_;
+  rclcpp::Service<autoware_adapi_v1_msgs::srv::SetRoute>::SharedPtr srv_set_route_;
+  rclcpp::Service<autoware_adapi_v1_msgs::srv::ClearRoute>::SharedPtr srv_clear_route_;
+  rclcpp::Service<autoware_adapi_v1_msgs::srv::ChangeOperationMode>::SharedPtr srv_stop_mode_;
+  rclcpp::Service<autoware_adapi_v1_msgs::srv::ChangeOperationMode>::SharedPtr srv_autonomous_mode_;
+
+  // Client
+  rclcpp::Client<autoware_adapi_v1_msgs::srv::InitializeLocalization>::SharedPtr cli_main_initialize_;
+  rclcpp::Client<autoware_adapi_v1_msgs::srv::InitializeLocalization>::SharedPtr cli_sub_initialize_;
+  rclcpp::Client<autoware_adapi_v1_msgs::srv::SetRoute>::SharedPtr cli_main_set_route_;
+  rclcpp::Client<autoware_adapi_v1_msgs::srv::SetRoute>::SharedPtr cli_sub_set_route_;
+  rclcpp::Client<autoware_adapi_v1_msgs::srv::ClearRoute>::SharedPtr cli_main_clear_route_;
+  rclcpp::Client<autoware_adapi_v1_msgs::srv::ClearRoute>::SharedPtr cli_sub_clear_route_;
+  rclcpp::Client<autoware_adapi_v1_msgs::srv::ChangeOperationMode>::SharedPtr cli_main_stop_mode_;
+  rclcpp::Client<autoware_adapi_v1_msgs::srv::ChangeOperationMode>::SharedPtr cli_sub_stop_mode_;
+  rclcpp::Client<autoware_adapi_v1_msgs::srv::ChangeOperationMode>::SharedPtr cli_main_autonomous_mode_;
+  rclcpp::Client<autoware_adapi_v1_msgs::srv::ChangeOperationMode>::SharedPtr cli_sub_autonomous_mode_;
+
+  // Function
+  void on_initialize(
+    const autoware_adapi_v1_msgs::srv::InitializeLocalization::Request::SharedPtr req,
+    const autoware_adapi_v1_msgs::srv::InitializeLocalization::Response::SharedPtr res);
+
+  void on_set_route(
+    const autoware_adapi_v1_msgs::srv::SetRoute::Request::SharedPtr req,
+    const autoware_adapi_v1_msgs::srv::SetRoute::Response::SharedPtr res);
+
+  void on_clear_route(
+    const autoware_adapi_v1_msgs::srv::ClearRoute::Request::SharedPtr req,
+    const autoware_adapi_v1_msgs::srv::ClearRoute::Response::SharedPtr res);
+
+  void on_change_to_stop(
+    const autoware_adapi_v1_msgs::srv::ChangeOperationMode::Request::SharedPtr req,
+    const autoware_adapi_v1_msgs::srv::ChangeOperationMode::Response::SharedPtr res);
+
+  void on_change_to_autonomous(
+    const autoware_adapi_v1_msgs::srv::ChangeOperationMode::Request::SharedPtr req,
+    const autoware_adapi_v1_msgs::srv::ChangeOperationMode::Response::SharedPtr res);
 };
 
 }  // namespace ad_api_distributor
