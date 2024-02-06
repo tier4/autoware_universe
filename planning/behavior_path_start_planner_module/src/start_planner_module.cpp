8c0de1c8e4bc425fe658d7efb0abde71c14de5d0
Kosuke Takeuchi <kosuke.tnp@gmail.com>
2024-02-06 21:34:48 +0900

    fix(start/goal_planner): fix multi thread memory crash (#6322)
    Signed-off-by: kosuke55 <kosuke.tnp@gmail.com>

--- planning/behavior_path_start_planner_module/src/start_planner_module.cpp
+++ planning/behavior_path_start_planner_module/src/start_planner_module.cpp
@@ -54,7 +54,8 @@ StartPlannerModule::StartPlannerModule(
     objects_of_interest_marker_interface_ptr_map)
 : SceneModuleInterface{name, node, rtc_interface_ptr_map, objects_of_interest_marker_interface_ptr_map},  // NOLINT
   parameters_{parameters},
-  vehicle_info_{vehicle_info_util::VehicleInfoUtil(node).getVehicleInfo()}
+  vehicle_info_{vehicle_info_util::VehicleInfoUtil(node).getVehicleInfo()},
+  is_freespace_planner_cb_running_{false}
 {
   lane_departure_checker_ = std::make_shared<LaneDepartureChecker>();
   lane_departure_checker_->setVehicleInfo(vehicle_info_);
@@ -85,6 +86,8 @@ StartPlannerModule::StartPlannerModule(
 
 void StartPlannerModule::onFreespacePlannerTimer()
 {
+  const ScopedFlag flag(is_freespace_planner_cb_running_);
+
   if (getCurrentStatus() == ModuleStatus::IDLE) {
     return;
   }
