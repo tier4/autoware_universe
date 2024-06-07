3d603d1d79856a99d930be5b12b6f1ab6a00f192
Shumpei Wakabayashi <42209144+shmpwk@users.noreply.github.com>
2024-06-07 18:40:41 +0900

    Update planning/motion_velocity_smoother/src/motion_velocity_smoother_node.cpp

--- planning/motion_velocity_smoother/src/motion_velocity_smoother_node.cpp
+++ planning/motion_velocity_smoother/src/motion_velocity_smoother_node.cpp
@@ -1143,7 +1143,7 @@ void MotionVelocitySmootherNode::onForceAcceleration(
     force_acceleration_mode_ = true;
     message = "Trigger force acceleration";
   } else if (!request->data && force_acceleration_mode_) {
-    RCLCPP_INFO(get_logger(), "Force accelration is deactivated");
+    RCLCPP_INFO(get_logger(), "Force acceleration is deactivated");
     smoother_->setMaxAccel(get_parameter("normal.max_acc").as_double());
     smoother_->setMaxJerk(get_parameter("normal.max_jerk").as_double());
     smoother_->setMaxLatAccel(get_parameter("max_lateral_accel").as_double());
