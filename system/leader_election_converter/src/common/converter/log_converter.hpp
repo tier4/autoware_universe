80930c45c0b38b205c1aa12ebc622a546ce5f610
TetsuKawa <kawaguchitnon@icloud.com>
2024-06-25 01:27:34 +0900

    feat: add leader election converter
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>

---
+++ system/leader_election_converter/src/common/converter/log_converter.hpp
@@ -0,0 +1,87 @@
+// Copyright 2024 The Autoware Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef COMMON__LOG_CONVERTER_HPP_
+#define COMMON__LOG_CONVERTER_HPP_
+
+#include <tier4_system_msgs/msg/election_communication.hpp>
+#include <tier4_system_msgs/msg/election_status.hpp>
+#include <tier4_system_msgs/msg/mrm_state.hpp>
+#include <autoware_adapi_v1_msgs/msg/mrm_state.hpp>
+
+#include <rclcpp/rclcpp.hpp>
+#include <thread>
+#include <atomic>
+
+#include "udp_sender.hpp"
+#include "udp_receiver.hpp"
+
+
+namespace leader_election_converter
+{
+
+typedef struct ElectionCommunication
+{
+  uint64_t msg;
+} ElectionCommunication;
+
+typedef struct ElectionStatus
+{
+  tier4_system_msgs::msg::ElectionStatus::_leader_id_type leader_id;
+  tier4_system_msgs::msg::ElectionStatus::_path_info_type path_info;
+  tier4_system_msgs::msg::MrmState::_state_type state;
+  tier4_system_msgs::msg::MrmBehavior::_type_type behavior;
+  tier4_system_msgs::msg::ElectionStatus::_election_start_count_type election_start_count;
+  tier4_system_msgs::msg::ElectionStatus::_in_election_type in_election;
+  tier4_system_msgs::msg::ElectionStatus::_is_received_availability_type is_received_availability;
+  tier4_system_msgs::msg::ElectionStatus::_is_received_mrm_state_type is_received_mrm_state;
+  tier4_system_msgs::msg::ElectionStatus::_is_autoware_emergency_type is_autoware_emergency;
+  tier4_system_msgs::msg::ElectionStatus::_is_main_ecu_connected_type is_main_ecu_connected;
+  tier4_system_msgs::msg::ElectionStatus::_is_sub_ecu_connected_type is_sub_ecu_connected;
+  tier4_system_msgs::msg::ElectionStatus::_is_main_vcu_connected_type is_main_vcu_connected;
+  tier4_system_msgs::msg::ElectionStatus::_is_sub_vcu_connected_type is_sub_vcu_connected;
+} ElectionStatus;
+
+class LogConverter
+{
+public:
+  LogConverter(rclcpp::Node * node);
+  ~LogConverter();
+
+  void setUdpElectionCommunicatioinReceiver(const std::string & src_ip, const std::string & src_port);
+  void setUdpElectionStatusReceiver(const std::string & src_ip, const std::string & src_port);
+  void setPublisher();
+
+private:
+  void startUdpElectionCommunicationReceiver(const std::string & src_ip, const std::string & src_port);
+  void startUdpElectionStatusReceiver(const std::string & src_ip, const std::string & src_port);
+  void convertElectionCommunicationToTopic(const ElectionCommunication & node_msg);
+  void convertElectionStatusToTopic(const ElectionStatus & status);
+
+  rclcpp::Node * node_;
+  std::unique_ptr<UdpReceiver<ElectionCommunication>> udp_election_comunication_receiver_;
+  std::unique_ptr<UdpReceiver<ElectionStatus>> udp_election_status_receiver_;
+  rclcpp::Publisher<tier4_system_msgs::msg::ElectionCommunication>::SharedPtr pub_election_comunication_;
+  rclcpp::Publisher<tier4_system_msgs::msg::ElectionStatus>::SharedPtr pub_election_status_;
+  rclcpp::Publisher<autoware_adapi_v1_msgs::msg::MrmState>::SharedPtr pub_over_all_mrm_state_;
+
+  std::thread udp_election_comunication_thread_;
+  std::thread udp_election_status_thread_;
+  std::atomic<bool> is_election_comunication_running_;
+  std::atomic<bool> is_election_status_running_;
+};
+
+}  // namespace leader_election_converter
+
+#endif  // COMMON__LOG_CONVERTER_HPP_
