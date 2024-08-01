a5097b4529499cf6579a5516006b6cadf800c7fa
Amadeusz Szymko <amadeusz.szymko.2@tier4.jp>
2024-08-01 10:04:54 +0900

    fix(autoware_lidar_centerpoint): place device vector in CUDA device system (#8272)
    Signed-off-by: amadeuszsz <amadeusz.szymko.2@tier4.jp>
    Signed-off-by: Kenzo Lobos-Tsunekawa <kenzo.lobos@tier4.jp>

--- perception/lidar_centerpoint/include/lidar_centerpoint/postprocess/postprocess_kernel.hpp
+++ perception/lidar_centerpoint/include/lidar_centerpoint/postprocess/postprocess_kernel.hpp
@@ -19,7 +19,6 @@
 #include "cuda_runtime_api.h"
 #include "lidar_centerpoint/centerpoint_config.hpp"
 #include "lidar_centerpoint/utils.hpp"
-#include "thrust/device_vector.h"
 
 #include <vector>
 
@@ -37,8 +36,6 @@ public:
 
 private:
   CenterPointConfig config_;
-  thrust::device_vector<Box3D> boxes3d_d_;
-  thrust::device_vector<float> yaw_norm_thresholds_d_;
 };
 
 }  // namespace centerpoint
