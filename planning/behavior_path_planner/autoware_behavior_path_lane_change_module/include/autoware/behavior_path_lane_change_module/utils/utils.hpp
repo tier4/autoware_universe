6115e7b9ba522bcd6fa80f606414981e6119ce65
mkquda <168697710+mkquda@users.noreply.github.com>
2024-09-27 14:59:11 +0900

    feat(lane_change): modify lane change target boundary check to consider velocity (#8961)
    * check if candidate path footprint exceeds target lane boundary when lc velocity is above minimum
    
    Signed-off-by: mohammad alqudah <alqudah.mohammad@tier4.jp>
    
    * move functions to relevant module
    
    Signed-off-by: mohammad alqudah <alqudah.mohammad@tier4.jp>
    
    * suppress unused function cppcheck
    
    Signed-off-by: mohammad alqudah <alqudah.mohammad@tier4.jp>
    
    * minor change
    
    Signed-off-by: mohammad alqudah <alqudah.mohammad@tier4.jp>
    
    ---------
    
    Signed-off-by: mohammad alqudah <alqudah.mohammad@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_lane_change_module/include/autoware/behavior_path_lane_change_module/utils/utils.hpp
+++ planning/behavior_path_planner/autoware_behavior_path_lane_change_module/include/autoware/behavior_path_lane_change_module/utils/utils.hpp
@@ -83,7 +83,7 @@ bool isPathInLanelets(
   const PathWithLaneId & path, const lanelet::ConstLanelets & current_lanes,
   const lanelet::ConstLanelets & target_lanes);
 
-bool pathFootprintExceedsTargetLaneBound(
+bool path_footprint_exceeds_target_lane_bound(
   const CommonDataPtr & common_data_ptr, const PathWithLaneId & path, const VehicleInfo & ego_info,
   const double margin = 0.1);
 
@@ -237,10 +237,6 @@ bool is_same_lane_with_prev_iteration(
   const CommonDataPtr & common_data_ptr, const lanelet::ConstLanelets & current_lanes,
   const lanelet::ConstLanelets & target_lanes);
 
-Pose to_pose(
-  const autoware::universe_utils::Point2d & point,
-  const geometry_msgs::msg::Quaternion & orientation);
-
 bool is_ahead_of_ego(
   const CommonDataPtr & common_data_ptr, const PathWithLaneId & path,
   const ExtendedPredictedObject & object);
@@ -372,14 +368,5 @@ bool filter_target_lane_objects(
   const bool before_terminal, TargetLaneLeadingObjects & leading_objects,
   ExtendedPredictedObjects & trailing_objects);
 }  // namespace autoware::behavior_path_planner::utils::lane_change
-
-namespace autoware::behavior_path_planner::utils::lane_change::debug
-{
-geometry_msgs::msg::Point32 create_point32(const geometry_msgs::msg::Pose & pose);
-
-geometry_msgs::msg::Polygon createExecutionArea(
-  const VehicleInfo & vehicle_info, const Pose & pose, double additional_lon_offset,
-  double additional_lat_offset);
-}  // namespace autoware::behavior_path_planner::utils::lane_change::debug
 
 #endif  // AUTOWARE__BEHAVIOR_PATH_LANE_CHANGE_MODULE__UTILS__UTILS_HPP_
