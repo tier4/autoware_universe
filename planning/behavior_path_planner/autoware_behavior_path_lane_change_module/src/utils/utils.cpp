b62a71f28870c4b85745803f3164329696aa862f
Zulfaqar Azmi <93502286+zulfaqar-azmi-t4@users.noreply.github.com>
2024-09-27 19:05:55 +0900

    refactor(lane_change): add TransientData to store commonly used lane change-related variables. (#8954)
    * add transient data
    
    Signed-off-by: Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
    
    * reverted max lc dist in  calcCurrentMinMax
    
    Signed-off-by: Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
    
    * rename
    
    Signed-off-by: Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
    
    * minor refactoring
    
    Signed-off-by: Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
    
    * update doxygen comments
    
    Signed-off-by: Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
    
    ---------
    
    Signed-off-by: Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_lane_change_module/src/utils/utils.cpp
+++ planning/behavior_path_planner/autoware_behavior_path_lane_change_module/src/utils/utils.cpp
@@ -577,6 +577,26 @@ double getLateralShift(const LaneChangePath & path)
   return path.shifted_path.shift_length.at(end_idx) - path.shifted_path.shift_length.at(start_idx);
 }
 
+bool hasEnoughLengthToLaneChangeAfterAbort(
+  const CommonDataPtr & common_data_ptr, const double abort_return_dist)
+{
+  const auto & current_lanes = common_data_ptr->lanes_ptr->current;
+  if (current_lanes.empty()) {
+    return false;
+  }
+
+  const auto & current_pose = common_data_ptr->get_ego_pose();
+  const auto abort_plus_lane_change_length =
+    abort_return_dist + common_data_ptr->transient_data.current_dist_buffer.min;
+  if (abort_plus_lane_change_length > utils::getDistanceToEndOfLane(current_pose, current_lanes)) {
+    return false;
+  }
+
+  const auto goal_pose = common_data_ptr->route_handler_ptr->getGoalPose();
+  return abort_plus_lane_change_length <=
+         utils::getSignedDistance(current_pose, goal_pose, current_lanes);
+}
+
 std::vector<std::vector<int64_t>> getSortedLaneIds(
   const RouteHandler & route_handler, const Pose & current_pose,
   const lanelet::ConstLanelets & current_lanes, const lanelet::ConstLanelets & target_lanes)
@@ -858,9 +878,7 @@ bool passed_parked_objects(
     lane_change_parameters.object_check_min_road_shoulder_width;
   const auto & object_shiftable_ratio_threshold =
     lane_change_parameters.object_shiftable_ratio_threshold;
-  const auto & current_lanes = common_data_ptr->lanes_ptr->current;
-  const auto current_lane_path =
-    route_handler.getCenterLinePath(current_lanes, 0.0, std::numeric_limits<double>::max());
+  const auto & current_lane_path = common_data_ptr->current_lanes_path;
 
   if (objects.empty() || lane_change_path.path.points.empty() || current_lane_path.points.empty()) {
     return true;
