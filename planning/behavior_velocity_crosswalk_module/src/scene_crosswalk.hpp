200781f28bf32a2fef7a5c5fab5eeec90e246652
Satoshi OTA <44889564+satoshi-ota@users.noreply.github.com>
2024-11-05 20:03:47 +0900

    fix(crosswalk): don't use vehicle stop checker to remove unnecessary callback (#9234)
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

--- planning/behavior_velocity_crosswalk_module/src/scene_crosswalk.hpp
+++ planning/behavior_velocity_crosswalk_module/src/scene_crosswalk.hpp
@@ -436,8 +436,6 @@ private:
   // Debug
   mutable DebugData debug_data_;
 
-  std::unique_ptr<motion_utils::VehicleStopChecker> vehicle_stop_checker_{nullptr};
-
   // Stop watch
   StopWatch<std::chrono::milliseconds> stop_watch_;
 
