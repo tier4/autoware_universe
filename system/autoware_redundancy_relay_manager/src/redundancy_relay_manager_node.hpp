edd79f42ecaac6b66a244d7b340a90237ba2e123
TetsuKawa <70682030+TetsuKawa@users.noreply.github.com>
2025-01-23 19:09:43 +0900

    feat(redundancy_relay_manager): add redundancy relay manager node (#1778)
    * feat: add base branch
    
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>
    
    * style(pre-commit): autofix
    
    * feat: add subscriber
    
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>
    
    * style(pre-commit): autofix
    
    * feat: add more subscriber
    
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>
    
    * modify: delete const
    
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>
    
    * style(pre-commit): autofix
    
    * feat: add service
    
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>
    
    * style(pre-commit): autofix
    
    * feat: repalace polling_subscriber
    
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>
    
    * style(pre-commit): autofix
    
    * feat: add schema
    
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>
    
    * style(pre-commit): autofix
    
    * modify: fix cherry pick mistake
    
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>
    
    * feat: add config_path to launch
    
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>
    
    * feat: add some arg to launch
    
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>
    
    ---------
    
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ system/autoware_redundancy_relay_manager/src/redundancy_relay_manager_node.hpp
@@ -0,0 +1,76 @@
+// Copyright 2025 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef REDUNDANCY_RELAY_MANAGER_NODE_HPP_
+#define REDUNDANCY_RELAY_MANAGER_NODE_HPP_
+
+// ROS 2 core
+#include <autoware/universe_utils/ros/polling_subscriber.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+#include <autoware_adapi_v1_msgs/msg/operation_mode_state.hpp>
+#include <tier4_system_msgs/msg/election_status.hpp>
+#include <tier4_system_msgs/srv/change_topic_relay_control.hpp>
+
+#include <string>
+
+namespace autoware::redundancy_relay_manager
+{
+struct NodeParam
+{
+  int service_timeout_ms;
+};
+
+class RedundancyRelayManager : public rclcpp::Node
+{
+public:
+  explicit RedundancyRelayManager(const rclcpp::NodeOptions & options);
+
+private:
+  // Params
+  NodeParam node_param_;
+  // Subscribers
+  autoware::universe_utils::InterProcessPollingSubscriber<
+    autoware_adapi_v1_msgs::msg::OperationModeState>
+    sub_operation_mode_state_{this, "~/input/operation_mode/state"};
+  rclcpp::Subscription<tier4_system_msgs::msg::ElectionStatus>::SharedPtr sub_main_election_status_;
+  rclcpp::Subscription<tier4_system_msgs::msg::ElectionStatus>::SharedPtr sub_sub_election_status_;
+
+  // Clients
+  rclcpp::CallbackGroup::SharedPtr client_relay_trajectory_group_;
+  rclcpp::Client<tier4_system_msgs::srv::ChangeTopicRelayControl>::SharedPtr
+    client_relay_trajectory_;
+  rclcpp::CallbackGroup::SharedPtr client_relay_pose_with_covariance_group_;
+  rclcpp::Client<tier4_system_msgs::srv::ChangeTopicRelayControl>::SharedPtr
+    client_relay_pose_with_covariance_;
+
+  // Callbacks
+  void onMainElectionStatus(const tier4_system_msgs::msg::ElectionStatus::SharedPtr msg);
+  void onSubElectionStatus(const tier4_system_msgs::msg::ElectionStatus::SharedPtr msg);
+
+  // State
+  bool is_relaying_;
+  bool is_stopped_by_main_;
+  tier4_system_msgs::msg::ElectionStatus::SharedPtr main_election_status_;
+  tier4_system_msgs::msg::ElectionStatus::SharedPtr sub_election_status_;
+
+  // Functions
+  void requestTopicRelayControl(
+    const bool relay_on,
+    const rclcpp::Client<tier4_system_msgs::srv::ChangeTopicRelayControl>::SharedPtr client,
+    std::string topic_name);
+};
+}  // namespace autoware::redundancy_relay_manager
+
+#endif  // REDUNDANCY_RELAY_MANAGER_NODE_HPP_
