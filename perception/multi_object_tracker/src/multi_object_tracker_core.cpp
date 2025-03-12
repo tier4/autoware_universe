1d3d61d1f491205d1b4a6efc575c2b7e73cb48d5
TadaKazuto <kazuto.tada@tier4.jp>
2025-03-12 18:04:44 +0900

    Try elapsed time

--- perception/multi_object_tracker/src/multi_object_tracker_core.cpp
+++ perception/multi_object_tracker/src/multi_object_tracker_core.cpp
@@ -507,9 +507,8 @@ void MultiObjectTracker::sanitizeTracker(
       }
 
       if (delete_candidate_tracker) {
-        /* erase only when prioritized one has a measurement or the other one doesn't */
-        if (sorted_list_tracker[i]->getNoMeasurementCount() <= 0 or 
-            sorted_list_tracker[j]->getNoMeasurementCount() > 0) {
+        /* erase only when prioritized one has later(or equal time) meas than the other one doesn't */
+        if (sorted_list_tracker[i]->getElapsedTimeFromLastUpdate() <= sorted_list_tracker[j]->getElapsedTimeFromLastUpdate()) {
           // Remove from original list_tracker
           list_tracker.remove(sorted_list_tracker[j]);
           // Remove from sorted list
