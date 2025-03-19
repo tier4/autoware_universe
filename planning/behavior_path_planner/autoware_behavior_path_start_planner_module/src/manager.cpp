autowarefoundation/Autoware.Universe

clothoid geometric parking
Signed-off-by: Takumi Ito <takumi.ito@tier4.jp>

planning/behavior_path_planner/autoware_behavior_path_start_planner_module/src/manager.cpp
@@ -126,6 +126,12 @@ void StartPlannerModuleManager::updateModuleParams(
     updateParam<double>(
       parameters, ns + "pull_out_max_steer_angle",
       p->parallel_parking_parameters.pull_out_max_steer_angle);
+    updateParam<double>(
+      parameters, ns + "pull_out_steer_rate_lim",
+      p->parallel_parking_parameters.pull_out_steer_rate_lim);
+    updateParam<bool>(
+      parameters, ns + "pull_out_use_clothoid",
+      p->parallel_parking_parameters.pull_out_use_clothoid);
     updateParam<bool>(parameters, ns + "enable_back", p->enable_back);
     updateParam<double>(parameters, ns + "backward_velocity", p->backward_velocity);
     updateParam<double>(

tier4/autoware_universe

planning/behavior_path_planner/autoware_behavior_path_start_planner_module/src/manager.cpp
@@ -129,6 +129,12 @@ void StartPlannerModuleManager::updateModuleParams(
     update_param<double>(
       parameters, ns + "pull_out_max_steer_angle",
       p->parallel_parking_parameters.pull_out_max_steer_angle);
+    update_param<double>(
+      parameters, ns + "pull_out_steer_rate_lim",
+      p->parallel_parking_parameters.pull_out_steer_rate_lim);
+    update_param<bool>(
+      parameters, ns + "pull_out_use_clothoid",
+      p->parallel_parking_parameters.pull_out_use_clothoid);
     update_param<bool>(parameters, ns + "enable_back", p->enable_back);
     update_param<double>(parameters, ns + "backward_velocity", p->backward_velocity);
     update_param<double>(