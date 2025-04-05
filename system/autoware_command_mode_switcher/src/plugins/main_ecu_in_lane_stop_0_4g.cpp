TetsuKawa/autoware.universe

feat: add in lane stop switcher
Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>

system/autoware_command_mode_switcher/src/switchers/main_ecu_in_lane_stop_0_4g.cpp
@@ -1,0 +1,25 @@
+//  Copyright 2025 The Autoware Contributors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#include "main_ecu_in_lane_stop_0_4g.hpp"
+
+namespace autoware::command_mode_switcher
+{
+
+}  // namespace autoware::command_mode_switcher
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(
+  autoware::command_mode_switcher::MainEcuInLaneStop04GSwitcher,
+  autoware::command_mode_switcher::SwitcherPlugin)

system/autoware_command_mode_switcher/src/switchers/main_ecu_in_lane_stop_0_4g.hpp
@@ -1,0 +1,34 @@
+//  Copyright 2025 The Autoware Contributors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#ifndef SWITCHERS__MAIN_ECU_IN_LANE_STOP_0_4G_HPP_
+#define SWITCHERS__MAIN_ECU_IN_LANE_STOP_0_4G_HPP_
+#include "common/plugin.hpp"
+
+#include <string>
+
+namespace autoware::command_mode_switcher
+{
+
+class MainEcuInLaneStop04GSwitcher : public SwitcherPlugin
+{
+public:
+  std::string mode_name() const override { return "main_ecu_in_lane_stop_0_4g"; }
+  std::string source_name() const override { return "main_ecu_in_lane_stop_0_4g"; }
+  bool autoware_control() const override { return true; }
+};
+
+}  // namespace autoware::command_mode_switcher
+
+#endif  //SWITCHERS__MAIN_ECU_IN_LANE_STOP_0_4G_HPP_

system/autoware_command_mode_switcher/src/switchers/main_ecu_in_lane_stop_0_6g.cpp
@@ -1,0 +1,25 @@
+//  Copyright 2025 The Autoware Contributors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#include "main_ecu_in_lane_stop_0_6g.hpp"
+
+namespace autoware::command_mode_switcher
+{
+
+}  // namespace autoware::command_mode_switcher
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(
+  autoware::command_mode_switcher::MainEcuInLaneStop06GSwitcher,
+  autoware::command_mode_switcher::SwitcherPlugin)

system/autoware_command_mode_switcher/src/switchers/main_ecu_in_lane_stop_0_6g.hpp
@@ -1,0 +1,34 @@
+//  Copyright 2025 The Autoware Contributors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#ifndef SWITCHERS__MAIN_ECU_IN_LANE_STOP_0_6G_HPP_
+#define SWITCHERS__MAIN_ECU_IN_LANE_STOP_0_6G_HPP_
+#include "common/plugin.hpp"
+
+#include <string>
+
+namespace autoware::command_mode_switcher
+{
+
+class MainEcuInLaneStop06GSwitcher : public SwitcherPlugin
+{
+public:
+  std::string mode_name() const override { return "main_ecu_in_lane_stop_0_6g"; }
+  std::string source_name() const override { return "main_ecu_in_lane_stop_0_6g"; }
+  bool autoware_control() const override { return true; }
+};
+
+}  // namespace autoware::command_mode_switcher
+
+#endif  //SWITCHERS__MAIN_ECU_IN_LANE_STOP_0_6G_HPP_

system/autoware_command_mode_switcher/src/switchers/sub_ecu_in_lane_stop_0_4g.cpp
@@ -1,0 +1,25 @@
+//  Copyright 2025 The Autoware Contributors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#include "sub_ecu_in_lane_stop_0_4g.hpp"
+
+namespace autoware::command_mode_switcher
+{
+
+}  // namespace autoware::command_mode_switcher
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(
+  autoware::command_mode_switcher::SubEcuInLaneStop04GSwitcher,
+  autoware::command_mode_switcher::SwitcherPlugin)

system/autoware_command_mode_switcher/src/switchers/sub_ecu_in_lane_stop_0_4g.hpp
@@ -1,0 +1,34 @@
+//  Copyright 2025 The Autoware Contributors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#ifndef SWITCHERS__SUB_ECU_IN_LANE_STOP_0_4G_HPP_
+#define SWITCHERS__SUB_ECU_IN_LANE_STOP_0_4G_HPP_
+#include "common/plugin.hpp"
+
+#include <string>
+
+namespace autoware::command_mode_switcher
+{
+
+class SubEcuInLaneStop04GSwitcher : public SwitcherPlugin
+{
+public:
+  std::string mode_name() const override { return "sub_ecu_in_lane_stop_0_4g"; }
+  std::string source_name() const override { return "sub_ecu_in_lane_stop_0_4g"; }
+  bool autoware_control() const override { return true; }
+};
+
+}  // namespace autoware::command_mode_switcher
+
+#endif  //SWITCHERS__SUB_ECU_IN_LANE_STOP_0_4G_HPP_

tier4/autoware_universe

system/autoware_command_mode_switcher/src/plugins/main_ecu_in_lane_stop_0_4g.cpp
@@ -1,0 +1,25 @@
+//  Copyright 2025 The Autoware Contributors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#include "main_ecu_in_lane_stop_0_4g.hpp"
+
+namespace autoware::command_mode_switcher
+{
+
+}  // namespace autoware::command_mode_switcher
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(
+  autoware::command_mode_switcher::MainEcuInLaneStop04GSwitcher,
+  autoware::command_mode_switcher::CommandPlugin)

system/autoware_command_mode_switcher/src/plugins/main_ecu_in_lane_stop_0_4g.hpp
@@ -1,0 +1,35 @@
+//  Copyright 2025 The Autoware Contributors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#ifndef SWITCHERS__MAIN_ECU_IN_LANE_STOP_0_4G_HPP_
+#define SWITCHERS__MAIN_ECU_IN_LANE_STOP_0_4G_HPP_
+
+#include "common/command_plugin.hpp"
+
+#include <string>
+
+namespace autoware::command_mode_switcher
+{
+
+class MainEcuInLaneStop04GSwitcher : public ControlCommandPlugin
+{
+public:
+  std::string mode_name() const override { return "main_ecu_in_lane_stop_0_4g"; }
+  std::string source_name() const override { return "main_ecu_in_lane_stop_0_4g"; }
+  bool autoware_control() const override { return true; }
+};
+
+}  // namespace autoware::command_mode_switcher
+
+#endif  //SWITCHERS__MAIN_ECU_IN_LANE_STOP_0_4G_HPP_

system/autoware_command_mode_switcher/src/plugins/main_ecu_in_lane_stop_0_6g.cpp
@@ -1,0 +1,25 @@
+//  Copyright 2025 The Autoware Contributors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#include "main_ecu_in_lane_stop_0_6g.hpp"
+
+namespace autoware::command_mode_switcher
+{
+
+}  // namespace autoware::command_mode_switcher
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(
+  autoware::command_mode_switcher::MainEcuInLaneStop06GSwitcher,
+  autoware::command_mode_switcher::CommandPlugin)

system/autoware_command_mode_switcher/src/plugins/main_ecu_in_lane_stop_0_6g.hpp
@@ -1,0 +1,35 @@
+//  Copyright 2025 The Autoware Contributors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#ifndef SWITCHERS__MAIN_ECU_IN_LANE_STOP_0_6G_HPP_
+#define SWITCHERS__MAIN_ECU_IN_LANE_STOP_0_6G_HPP_
+
+#include "common/command_plugin.hpp"
+
+#include <string>
+
+namespace autoware::command_mode_switcher
+{
+
+class MainEcuInLaneStop06GSwitcher : public ControlCommandPlugin
+{
+public:
+  std::string mode_name() const override { return "main_ecu_in_lane_stop_0_6g"; }
+  std::string source_name() const override { return "main_ecu_in_lane_stop_0_6g"; }
+  bool autoware_control() const override { return true; }
+};
+
+}  // namespace autoware::command_mode_switcher
+
+#endif  //SWITCHERS__MAIN_ECU_IN_LANE_STOP_0_6G_HPP_

system/autoware_command_mode_switcher/src/plugins/sub_ecu_in_lane_stop_0_4g.cpp
@@ -1,0 +1,25 @@
+//  Copyright 2025 The Autoware Contributors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#include "sub_ecu_in_lane_stop_0_4g.hpp"
+
+namespace autoware::command_mode_switcher
+{
+
+}  // namespace autoware::command_mode_switcher
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(
+  autoware::command_mode_switcher::SubEcuInLaneStop04GSwitcher,
+  autoware::command_mode_switcher::CommandPlugin)

system/autoware_command_mode_switcher/src/plugins/sub_ecu_in_lane_stop_0_4g.hpp
@@ -1,0 +1,35 @@
+//  Copyright 2025 The Autoware Contributors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#ifndef SWITCHERS__SUB_ECU_IN_LANE_STOP_0_4G_HPP_
+#define SWITCHERS__SUB_ECU_IN_LANE_STOP_0_4G_HPP_
+
+#include "common/command_plugin.hpp"
+
+#include <string>
+
+namespace autoware::command_mode_switcher
+{
+
+class SubEcuInLaneStop04GSwitcher : public ControlCommandPlugin
+{
+public:
+  std::string mode_name() const override { return "sub_ecu_in_lane_stop_0_4g"; }
+  std::string source_name() const override { return "sub_ecu_in_lane_stop_0_4g"; }
+  bool autoware_control() const override { return true; }
+};
+
+}  // namespace autoware::command_mode_switcher
+
+#endif  //SWITCHERS__SUB_ECU_IN_LANE_STOP_0_4G_HPP_