baac83fb772781f24fcf9ef2c092abcceb67b374
Takagi, Isamu <isamu.takagi@tier4.jp>
2025-04-07 09:13:27 +0900

    update decider
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- system/autoware_command_mode_decider/src/command_mode_decider_base.cpp
+++ system/autoware_command_mode_decider/src/command_mode_decider_base.cpp
@@ -23,6 +23,17 @@
 namespace autoware::command_mode_decider
 {
 
+void logging_mode_change(
+  const rclcpp::Logger & logger, const std::string & name, const std::string & prev,
+  const std::string & next)
+{
+  if (prev != next) {
+    const auto prev_text = "'" + prev + "'";
+    const auto next_text = "'" + next + "'";
+    RCLCPP_INFO_STREAM(logger, name << " mode changed: " << prev_text << " -> " << next_text);
+  }
+}
+
 CommandModeDeciderBase::CommandModeDeciderBase(const rclcpp::NodeOptions & options)
 : Node("command_mode_decider", options)
 {
@@ -36,11 +47,14 @@ CommandModeDeciderBase::CommandModeDeciderBase(const rclcpp::NodeOptions & optio
   system_request_.mrm = "";
   foreground_request_ = "";
   background_request_ = "";
+  command_mode_ = "";
   request_mode_ = "";
   request_stamp_ = std::nullopt;
 
   curr_autoware_control_ = false;
+  last_autoware_control_ = false;
   curr_operation_mode_ = "";
+  last_operation_mode_ = "";
 
   using std::placeholders::_1;
   using std::placeholders::_2;
@@ -68,6 +82,15 @@ CommandModeDeciderBase::CommandModeDeciderBase(const rclcpp::NodeOptions & optio
 
   const auto period = rclcpp::Rate(declare_parameter<double>("update_rate")).period();
   timer_ = rclcpp::create_timer(this, get_clock(), period, [this]() { on_timer(); });
+}
+
+bool CommandModeDeciderBase::is_in_transition() const
+{
+  if (system_request_.autoware_control) {
+    return last_operation_mode_ != system_request_.operation_mode;
+  } else {
+    return last_autoware_control_ != system_request_.autoware_control;
+  }
 }
 
 void CommandModeDeciderBase::on_status(const CommandModeStatus & msg)
@@ -117,15 +140,9 @@ void CommandModeDeciderBase::update_request_mode()
 {
   // Decide command mode with system-dependent logic.
   {
-    const auto prev_mode = request_mode_;
-    const auto next_mode = decide_command_mode();
-    request_mode_ = next_mode;
-
-    if (prev_mode != next_mode) {
-      const auto prev_text = "'" + prev_mode + "'";
-      const auto next_text = "'" + next_mode + "'";
-      RCLCPP_INFO_STREAM(get_logger(), "mode changed: " << prev_text << " -> " << next_text);
-    }
+    const auto mode = decide_command_mode();
+    logging_mode_change(get_logger(), "request", request_mode_, mode);
+    request_mode_ = mode;
   }
 
   // Convert the request into internal structure.
@@ -140,18 +157,48 @@ void CommandModeDeciderBase::update_request_mode()
 
 void CommandModeDeciderBase::update_current_mode()
 {
-  // Check current manual control.
-  {
-    const auto status = command_mode_status_.get(manual_mode_name_);
-    curr_autoware_control_ = !status.check_gate_ready(GateType::VehicleGate);
+  // Update current command mode.
+  for (const auto & [mode, status] : command_mode_status_) {
+    if (status.check_gate_ready(GateType::VehicleGate)) {
+      logging_mode_change(get_logger(), "command", command_mode_, mode);
+      command_mode_ = mode;
+      break;
+    }
   }
-  // Check current operation mode
+
+  // Update current operation mode
   for (const auto & [mode, status] : command_mode_status_) {
     if (command_to_operation_mode(mode) == OperationModeState::UNKNOWN) {
       continue;
     }
     if (status.check_gate_ready(GateType::ControlGate)) {
       curr_operation_mode_ = mode;
+      break;
+    }
+  }
+
+  // Update current manual control.
+  {
+    const auto status = command_mode_status_.get(manual_mode_name_);
+    curr_autoware_control_ = !status.check_gate_ready(GateType::VehicleGate);
+  }
+
+  // Update last operator (manual control or autoware operation mode).
+  if (is_in_transition()) {
+    if (system_request_.autoware_control) {
+      const auto status = command_mode_status_.get(system_request_.operation_mode);
+      if (status.check_mode_ready()) {
+        last_autoware_control_ = true;
+        last_operation_mode_ = system_request_.operation_mode;
+        RCLCPP_INFO_STREAM(get_logger(), "last mode: " << last_operation_mode_);
+      }
+    } else {
+      const auto status = command_mode_status_.get(manual_mode_name_);
+      if (status.check_mode_ready()) {
+        last_autoware_control_ = false;
+        last_operation_mode_ = "";
+        RCLCPP_INFO_STREAM(get_logger(), "last mode: manual");
+      }
     }
   }
 }
@@ -201,7 +248,7 @@ void CommandModeDeciderBase::publish_operation_mode_state()
   state.stamp = now();
   state.mode = command_to_operation_mode(curr_operation_mode_);
   state.is_autoware_control_enabled = curr_autoware_control_;
-  state.is_in_transition = false;  // TODO(Takagi, Isamu): implement
+  state.is_in_transition = is_in_transition();
   state.is_stop_mode_available = is_transition_available("stop");
   state.is_autonomous_mode_available = is_transition_available("autonomous");
   state.is_local_mode_available = is_transition_available("local");
