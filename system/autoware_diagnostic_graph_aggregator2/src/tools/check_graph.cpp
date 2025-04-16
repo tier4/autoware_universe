50fc001fb773b80b5c2a1952bc38c34ea827fede
Takagi, Isamu <isamu.takagi@tier4.jp>
2025-04-16 19:50:24 +0900

    create temporary package
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

---
+++ system/autoware_diagnostic_graph_aggregator2/src/tools/check_graph.cpp
@@ -0,0 +1,36 @@
+// Copyright 2023 The Autoware Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "check_graph.hpp"
+
+#include "config/loader.hpp"
+
+#include <string>
+
+namespace autoware::diagnostic_graph_aggregator
+{
+
+void check_graph(const std::string & path)
+{
+  load_file(path);
+}
+
+}  // namespace autoware::diagnostic_graph_aggregator
+
+int main(int argc, char ** argv)
+{
+  (void)argc;
+  (void)argv;
+  autoware::diagnostic_graph_aggregator::check_graph("config/graph.yaml");
+}
