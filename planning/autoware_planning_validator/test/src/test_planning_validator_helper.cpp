beaafb58697d253f071b4bb8b4de280930380208
mohammad alqudah <alqudah.mohammad@tier4.jp>
2025-04-21 14:22:28 +0900

    fix node interface tests and pubsub tests
    Signed-off-by: mohammad alqudah <alqudah.mohammad@tier4.jp>

--- planning/autoware_planning_validator/test/src/test_planning_validator_helper.cpp
+++ planning/autoware_planning_validator/test/src/test_planning_validator_helper.cpp
@@ -210,6 +210,13 @@ Odometry generateDefaultOdometry(const double x, const double y, const double vx
   return odom;
 }
 
+AccelWithCovarianceStamped generateDefaultAcceleration(const double ax)
+{
+  AccelWithCovarianceStamped acceleration;
+  acceleration.accel.accel.linear.x = ax;
+  return acceleration;
+}
+
 rclcpp::NodeOptions getNodeOptionsWithDefaultParams()
 {
   rclcpp::NodeOptions node_options;
