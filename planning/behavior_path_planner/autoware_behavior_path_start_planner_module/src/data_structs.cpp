autowarefoundation/Autoware.Universe

append enable_clothoid_* param instead of *_use_clothoid
Signed-off-by: Takumi Ito <takumi.ito@tier4.jp>

planning/behavior_path_planner/autoware_behavior_path_start_planner_module/src/data_structs.cpp
@@ -72,6 +72,7 @@ StartPlannerParameters StartPlannerParameters::init(rclcpp::Node & node)
     // geometric pull out
     p.enable_geometric_pull_out =
       getOrDeclareParameter<bool>(node, ns + "enable_geometric_pull_out");
+    p.enable_clothoid_pull_out = getOrDeclareParameter<bool>(node, ns + "enable_clothoid_pull_out");
     p.geometric_collision_check_distance_from_end =
       getOrDeclareParameter<double>(node, ns + "geometric_collision_check_distance_from_end");
     p.divide_pull_out_path = getOrDeclareParameter<bool>(node, ns + "divide_pull_out_path");

planning/behavior_path_planner/autoware_behavior_path_start_planner_module/src/manager.cpp
@@ -113,6 +113,7 @@ void StartPlannerModuleManager::updateModuleParams(
     updateParam<double>(
       parameters, ns + "maximum_longitudinal_deviation", p->maximum_longitudinal_deviation);
     updateParam<bool>(parameters, ns + "enable_geometric_pull_out", p->enable_geometric_pull_out);
+    updateParam<bool>(parameters, ns + "enable_clothoid_pull_out", p->enable_clothoid_pull_out);
     updateParam<bool>(parameters, ns + "divide_pull_out_path", p->divide_pull_out_path);
     updateParam<double>(
       parameters, ns + "arc_path_interval",
@@ -129,9 +130,6 @@ void StartPlannerModuleManager::updateModuleParams(
     updateParam<double>(
       parameters, ns + "pull_out_steer_rate_lim",
       p->parallel_parking_parameters.pull_out_steer_rate_lim);
-    updateParam<bool>(
-      parameters, ns + "pull_out_use_clothoid",
-      p->parallel_parking_parameters.pull_out_use_clothoid);
     updateParam<bool>(parameters, ns + "enable_back", p->enable_back);
     updateParam<double>(parameters, ns + "backward_velocity", p->backward_velocity);
     updateParam<double>(

tier4/autoware_universe

planning/behavior_path_planner/autoware_behavior_path_start_planner_module/src/data_structs.cpp
@@ -73,6 +73,7 @@ StartPlannerParameters StartPlannerParameters::init(rclcpp::Node & node)
     // geometric pull out
     p.enable_geometric_pull_out =
       get_or_declare_parameter<bool>(node, ns + "enable_geometric_pull_out");
+    p.enable_clothoid_pull_out = get_or_declare_parameter<bool>(node, ns + "enable_clothoid_pull_out");
     p.geometric_collision_check_distance_from_end =
       get_or_declare_parameter<double>(node, ns + "geometric_collision_check_distance_from_end");
     p.divide_pull_out_path = get_or_declare_parameter<bool>(node, ns + "divide_pull_out_path");

planning/behavior_path_planner/autoware_behavior_path_start_planner_module/src/manager.cpp
@@ -116,6 +116,7 @@ void StartPlannerModuleManager::updateModuleParams(
     update_param<double>(
       parameters, ns + "maximum_longitudinal_deviation", p->maximum_longitudinal_deviation);
     update_param<bool>(parameters, ns + "enable_geometric_pull_out", p->enable_geometric_pull_out);
+    update_param<bool>(parameters, ns + "enable_clothoid_pull_out", p->enable_clothoid_pull_out);
     update_param<bool>(parameters, ns + "divide_pull_out_path", p->divide_pull_out_path);
     update_param<double>(
       parameters, ns + "arc_path_interval",
@@ -134,9 +135,6 @@ void StartPlannerModuleManager::updateModuleParams(
     update_param<double>(
       parameters, ns + "pull_out_steer_rate_lim",
       p->parallel_parking_parameters.pull_out_steer_rate_lim);
-    update_param<bool>(
-      parameters, ns + "pull_out_use_clothoid",
-      p->parallel_parking_parameters.pull_out_use_clothoid);
     update_param<bool>(parameters, ns + "enable_back", p->enable_back);
     update_param<double>(parameters, ns + "backward_velocity", p->backward_velocity);
     update_param<double>(