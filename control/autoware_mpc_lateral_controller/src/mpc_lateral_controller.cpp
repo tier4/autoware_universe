2962afd138765146d77194ca66f37fe7ab3aa617
NickYang-Turing <nickyang@turing-drive.com>
2025-04-23 14:43:01 +0800

    Revert "fix(autoware_mpc_lateral_controller): remove is_mpc_solved for is_steer_converged temporally"
    This reverts commit 43e39f2b79fcb62e0a2285c5bbe8300b6f2e144f.

--- control/autoware_mpc_lateral_controller/src/mpc_lateral_controller.cpp
+++ control/autoware_mpc_lateral_controller/src/mpc_lateral_controller.cpp
@@ -318,7 +318,8 @@ trajectory_follower::LateralOutput MpcLateralController::run(
     // 1. At the last loop, mpc should be solved because command should be optimized output.
     // 2. The mpc should be converged.
     // 3. The steer angle should be converged.
-    output.sync_data.is_steer_converged = isMpcConverged() && isSteerConverged(cmd);
+    output.sync_data.is_steer_converged =
+      is_mpc_solved && isMpcConverged() && isSteerConverged(cmd);
 
     return output;
   };
