90bc570751b43fb29e8e8f7c2149719a9ea6ae83
satoshi-ota <satoshi.ota928@gmail.com>
2025-06-06 08:55:39 +0900

    Revert "implement function to check for sudden shift in trajectory"
    This reverts commit 2c18a87b13718be7268d6598d9fd840ffe5bd1ea.

--- planning/autoware_planning_validator/include/autoware/planning_validator/planning_validator.hpp
+++ planning/autoware_planning_validator/include/autoware/planning_validator/planning_validator.hpp
@@ -69,7 +69,6 @@ public:
   bool checkValidDistanceDeviation(const Trajectory & trajectory);
   bool checkValidLongitudinalDistanceDeviation(const Trajectory & trajectory);
   bool checkValidForwardTrajectoryLength(const Trajectory & trajectory);
-  bool checkValidLatency(const Trajectory & trajectory);
   bool checkTrajectoryShift(
     const Trajectory & trajectory, const Trajectory & prev_trajectory,
     const geometry_msgs::msg::Pose & ego_pose);
