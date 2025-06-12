9292e1183d578ecb518e7eddc90a2f4e9891b27d
Mamoru Sobue <mamoru.sobue@tier4.jp>
2025-06-12 17:40:37 +0900

    feat(goal_planner): consider lane changing previous module path
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_goal_planner_module/include/autoware/behavior_path_goal_planner_module/thread_data.hpp
+++ planning/behavior_path_planner/autoware_behavior_path_goal_planner_module/include/autoware/behavior_path_goal_planner_module/thread_data.hpp
@@ -45,7 +45,7 @@ public:
     const PlannerData & planner_data, const ModuleStatus & current_status,
     const BehaviorModuleOutput & upstream_module_output,
     const std::optional<PullOverPath> & pull_over_path, const PathDecisionState & prev_data,
-    const bool trigger_thread_on_approach);
+    const bool trigger_thread_on_approach, const bool lane_change_status_changed);
 
   const autoware_utils::LinearRing2d vehicle_footprint_;
   const GoalCandidates goal_candidates_;
@@ -60,6 +60,7 @@ public:
   const std::optional<PullOverPath> & get_pull_over_path() const { return pull_over_path_; }
   const PathDecisionState & get_prev_data() const { return prev_data_; }
   bool trigger_thread_on_approach() const { return trigger_thread_on_approach_; }
+  bool lane_change_status_changed() const { return lane_change_status_changed_; }
 
 private:
   std::shared_ptr<PlannerData> planner_data_;
@@ -68,6 +69,7 @@ private:
   std::optional<PullOverPath> pull_over_path_;  //<! pull over path selected by main thread
   PathDecisionState prev_data_;
   bool trigger_thread_on_approach_{false};
+  bool lane_change_status_changed_{false};
 };
 
 struct LaneParkingResponse
