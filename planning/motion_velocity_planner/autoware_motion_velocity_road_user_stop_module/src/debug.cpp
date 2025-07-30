40e55cfd4b19bfa9474d1c1ea80b88b310e04657
Kotakku <kotakkucu@gmail.com>
2025-07-30 14:07:53 +0900

    minimum debug marker
    Signed-off-by: Kotakku <kotakkucu@gmail.com>

--- planning/motion_velocity_planner/autoware_motion_velocity_road_user_stop_module/src/debug.cpp
+++ planning/motion_velocity_planner/autoware_motion_velocity_road_user_stop_module/src/debug.cpp
@@ -55,166 +55,91 @@ MarkerArray RoadUserStopModule::create_debug_marker_array() const
   autoware_utils_debug::ScopedTimeTrack st_debug("create_debug_marker_array", *time_keeper_);
   MarkerArray debug_marker_array;
 
-  // if (!debug_data_.ego_lanelets.empty()) {
-  //   autoware_utils_debug::ScopedTimeTrack st_debug(
-  //     "create_debug_marker_array/ego_lanelets", *time_keeper_);
-  //   std_msgs::msg::ColorRGBA pink_color;
-  //   pink_color.r = 1.0;
-  //   pink_color.g = 0.0;
-  //   pink_color.b = 1.0;
-  //   pink_color.a = 0.999;
-  //   const auto ego_lanelets_markers = lanelet::visualization::laneletsAsTriangleMarkerArray(
-  //     "ego_lanelets", debug_data_.ego_lanelets, pink_color);
+  if (!debug_data_.ego_lanelets.empty()) {
+    autoware_utils_debug::ScopedTimeTrack st_debug(
+      "create_debug_marker_array/ego_lanelets", *time_keeper_);
+    std_msgs::msg::ColorRGBA pink_color;
+    pink_color.r = 1.0;
+    pink_color.g = 0.0;
+    pink_color.b = 1.0;
+    pink_color.a = 0.999;
+    const auto ego_lanelets_markers = lanelet::visualization::laneletsAsTriangleMarkerArray(
+      "ego_lanelets", debug_data_.ego_lanelets, pink_color);
 
-  //   appendMarkerArray(ego_lanelets_markers, &debug_marker_array);
-  // }
+    appendMarkerArray(ego_lanelets_markers, &debug_marker_array);
+  }
 
-  // if (!debug_data_.ego_lanelets_without_intersection.empty()) {
-  //   autoware_utils_debug::ScopedTimeTrack st_debug(
-  //     "create_debug_marker_array/ego_lanelets_without_intersection", *time_keeper_);
-  //   std_msgs::msg::ColorRGBA pink_color;
-  //   pink_color.r = 0.0;
-  //   pink_color.g = 1.0;
-  //   pink_color.b = 0.0;
-  //   pink_color.a = 0.999;
-  //   const auto ego_lanelets_without_intersection_markers =
-  //     lanelet::visualization::laneletsAsTriangleMarkerArray(
-  //       "ego_lanelets_without_intersection", debug_data_.ego_lanelets_without_intersection,
-  //       pink_color);
+  if (!debug_data_.trajectory_polygons.empty()) {
+    autoware_utils_debug::ScopedTimeTrack st_debug(
+      "create_debug_marker_array/trajectory_polygons", *time_keeper_);
+    int traj_poly_id = 0;
+    for (const auto & polygon : debug_data_.trajectory_polygons) {
+      Marker traj_poly_marker = createDefaultMarker(
+        "map", clock_->now(), "trajectory_polygons", traj_poly_id++, Marker::LINE_STRIP,
+        autoware::universe_utils::createMarkerScale(0.1, 0, 0),
+        autoware::universe_utils::createMarkerColor(1.0, 1.0, 0.0, 0.5));  // Yellow color
 
-  //   appendMarkerArray(ego_lanelets_without_intersection_markers, &debug_marker_array);
-  // }
+      traj_poly_marker.lifetime = rclcpp::Duration::from_seconds(0.3);
 
-  // if (!debug_data_.adjacent_lanelets.empty()) {
-  //   autoware_utils_debug::ScopedTimeTrack st_debug(
-  //     "create_debug_marker_array/adjacent_lanelets", *time_keeper_);
-  //   std_msgs::msg::ColorRGBA orange_color;
-  //   orange_color.r = 1.0;
-  //   orange_color.g = 0.5;
-  //   orange_color.b = 0.0;
-  //   orange_color.a = 0.999;
-  //   const auto adjacent_lanelets_markers = lanelet::visualization::laneletsBoundaryAsMarkerArray(
-  //     debug_data_.adjacent_lanelets, orange_color, false, "adjacent_lanelets_");
-  //   appendMarkerArray(adjacent_lanelets_markers, &debug_marker_array);
-  // }
+      add_polygon_to_marker(traj_poly_marker, polygon);
 
-  // if (!debug_data_.trajectory_polygons.empty()) {
-  //   autoware_utils_debug::ScopedTimeTrack st_debug(
-  //     "create_debug_marker_array/trajectory_polygons", *time_keeper_);
-  //   int traj_poly_id = 0;
-  //   for (const auto & polygon : debug_data_.trajectory_polygons) {
-  //     Marker traj_poly_marker = createDefaultMarker(
-  //       "map", clock_->now(), "trajectory_polygons", traj_poly_id++, Marker::LINE_STRIP,
-  //       autoware::universe_utils::createMarkerScale(0.1, 0, 0),
-  //       autoware::universe_utils::createMarkerColor(1.0, 1.0, 0.0, 0.5));  // Yellow color
+      debug_marker_array.markers.push_back(traj_poly_marker);
+    }
+  }
 
-  //     traj_poly_marker.lifetime = rclcpp::Duration::from_seconds(0.3);
+  if (!debug_data_.object_polygons.empty()) {
+    autoware_utils_debug::ScopedTimeTrack st_debug(
+      "create_debug_marker_array/object_polygons", *time_keeper_);
+    int obj_poly_id = 0;
+    for (const auto & polygon : debug_data_.object_polygons) {
+      Marker obj_poly_marker = createDefaultMarker(
+        "map", clock_->now(), "object_polygons", obj_poly_id++, Marker::LINE_STRIP,
+        autoware::universe_utils::createMarkerScale(0.15, 0, 0),
+        autoware::universe_utils::createMarkerColor(0.8, 0.0, 0.8, 0.9));
 
-  //     add_polygon_to_marker(traj_poly_marker, polygon);
+      obj_poly_marker.lifetime = rclcpp::Duration::from_seconds(0.3);
 
-  //     debug_marker_array.markers.push_back(traj_poly_marker);
-  //   }
-  // }
+      add_polygon_to_marker(obj_poly_marker, polygon);
 
-  // if (debug_data_.stop_point) {
-  //   autoware_utils_debug::ScopedTimeTrack st_debug(
-  //     "create_debug_marker_array/stop_point", *time_keeper_);
-  //   Marker stop_marker = createDefaultMarker(
-  //     "map", clock_->now(), "stop_point", 0, Marker::SPHERE,
-  //     autoware::universe_utils::createMarkerScale(1.0, 1.0, 1.0),
-  //     autoware::universe_utils::createMarkerColor(1.0, 0.0, 0.0, 0.999));
-  //   stop_marker.lifetime = rclcpp::Duration::from_seconds(0.3);
-  //   stop_marker.pose.position = debug_data_.stop_point.value();
-  //   stop_marker.pose.orientation.w = 1.0;
-  //   debug_marker_array.markers.push_back(stop_marker);
-  // }
+      debug_marker_array.markers.push_back(obj_poly_marker);
+    }
+  }
 
-  // int obj_id = 0;
-  // for (const auto & object : debug_data_.filtered_objects) {
-  //   autoware_utils_debug::ScopedTimeTrack st_debug(
-  //     "create_debug_marker_array/filtered_objects", *time_keeper_);
-  //   Marker obj_marker = createDefaultMarker(
-  //     "map", clock_->now(), "filtered_objects", obj_id++, Marker::CYLINDER,
-  //     autoware::universe_utils::createMarkerScale(0.5, 0.5, 2.0),
-  //     autoware::universe_utils::createMarkerColor(0.0, 0.0, 1.0, 0.8));
+  if (!debug_data_.polygons_for_vru.empty()) {
+    autoware_utils_debug::ScopedTimeTrack st_debug(
+      "create_debug_marker_array/polygons_for_vru", *time_keeper_);
+    int vru_poly_id = 0;
+    for (const auto & polygon : debug_data_.polygons_for_vru) {
+      Marker vru_poly_marker = createDefaultMarker(
+        "map", clock_->now(), "polygons_for_vru", vru_poly_id++, Marker::LINE_STRIP,
+        autoware::universe_utils::createMarkerScale(0.15, 0, 0),
+        autoware::universe_utils::createMarkerColor(0.5, 1.0, 0.5, 0.6));
 
-  //   obj_marker.lifetime = rclcpp::Duration::from_seconds(0.3);
-  //   obj_marker.pose = object.kinematics.initial_pose_with_covariance.pose;
+      vru_poly_marker.lifetime = rclcpp::Duration::from_seconds(0.3);
 
-  //   debug_marker_array.markers.push_back(obj_marker);
-  // }
+      add_polygon_to_marker(vru_poly_marker, polygon);
 
-  // if (!debug_data_.object_polygons.empty()) {
-  //   autoware_utils_debug::ScopedTimeTrack st_debug(
-  //     "create_debug_marker_array/object_polygons", *time_keeper_);
-  //   int obj_poly_id = 0;
-  //   for (const auto & polygon : debug_data_.object_polygons) {
-  //     Marker obj_poly_marker = createDefaultMarker(
-  //       "map", clock_->now(), "object_polygons", obj_poly_id++, Marker::LINE_STRIP,
-  //       autoware::universe_utils::createMarkerScale(0.15, 0, 0),
-  //       autoware::universe_utils::createMarkerColor(0.8, 0.0, 0.8, 0.9));  // Purple color
+      debug_marker_array.markers.push_back(vru_poly_marker);
+    }
+  }
 
-  //     obj_poly_marker.lifetime = rclcpp::Duration::from_seconds(0.3);
+  if (!debug_data_.polygons_for_opposing_traffic.empty()) {
+    autoware_utils_debug::ScopedTimeTrack st_debug(
+      "create_debug_marker_array/polygons_for_opposing_traffic", *time_keeper_);
+    int opposing_poly_id = 0;
+    for (const auto & polygon : debug_data_.polygons_for_opposing_traffic) {
+      Marker opposing_poly_marker = createDefaultMarker(
+        "map", clock_->now(), "polygons_for_opposing_traffic", opposing_poly_id++,
+        Marker::LINE_STRIP, autoware::universe_utils::createMarkerScale(0.15, 0, 0),
+        autoware::universe_utils::createMarkerColor(1.0, 0.5, 0.5, 0.6));
 
-  //     add_polygon_to_marker(obj_poly_marker, polygon);
+      opposing_poly_marker.lifetime = rclcpp::Duration::from_seconds(0.3);
 
-  //     debug_marker_array.markers.push_back(obj_poly_marker);
-  //   }
-  // }
+      add_polygon_to_marker(opposing_poly_marker, polygon);
 
-  // if (!debug_data_.intersection_polygons.empty()) {
-  //   autoware_utils_debug::ScopedTimeTrack st_debug(
-  //     "create_debug_marker_array/intersection_polygons", *time_keeper_);
-  //   int intersection_poly_id = 0;
-  //   for (const auto & polygon : debug_data_.intersection_polygons) {
-  //     Marker intersection_poly_marker = createDefaultMarker(
-  //       "map", clock_->now(), "intersection_polygons", intersection_poly_id++,
-  //       Marker::LINE_STRIP, autoware::universe_utils::createMarkerScale(0.2, 0, 0),
-  //       autoware::universe_utils::createMarkerColor(0.0, 1.0, 1.0, 0.8));  // Cyan color
-
-  //     intersection_poly_marker.lifetime = rclcpp::Duration::from_seconds(0.3);
-
-  //     add_polygon_to_marker(intersection_poly_marker, polygon);
-
-  //     debug_marker_array.markers.push_back(intersection_poly_marker);
-  //   }
-  // }
-
-  // if (!debug_data_.polygons_for_vru.empty()) {
-  //   autoware_utils_debug::ScopedTimeTrack st_debug(
-  //     "create_debug_marker_array/polygons_for_vru", *time_keeper_);
-  //   int vru_poly_id = 0;
-  //   for (const auto & polygon : debug_data_.polygons_for_vru) {
-  //     Marker vru_poly_marker = createDefaultMarker(
-  //       "map", clock_->now(), "polygons_for_vru", vru_poly_id++, Marker::LINE_STRIP,
-  //       autoware::universe_utils::createMarkerScale(0.15, 0, 0),
-  //       autoware::universe_utils::createMarkerColor(0.5, 1.0, 0.5, 0.6));  // Light green color
-
-  //     vru_poly_marker.lifetime = rclcpp::Duration::from_seconds(0.3);
-
-  //     add_polygon_to_marker(vru_poly_marker, polygon);
-
-  //     debug_marker_array.markers.push_back(vru_poly_marker);
-  //   }
-  // }
-
-  // if (!debug_data_.polygons_for_opposing_traffic.empty()) {
-  //   autoware_utils_debug::ScopedTimeTrack st_debug(
-  //     "create_debug_marker_array/polygons_for_opposing_traffic", *time_keeper_);
-  //   int opposing_poly_id = 0;
-  //   for (const auto & polygon : debug_data_.polygons_for_opposing_traffic) {
-  //     Marker opposing_poly_marker = createDefaultMarker(
-  //       "map", clock_->now(), "polygons_for_opposing_traffic", opposing_poly_id++,
-  //       Marker::LINE_STRIP, autoware::universe_utils::createMarkerScale(0.15, 0, 0),
-  //       autoware::universe_utils::createMarkerColor(1.0, 0.5, 0.5, 0.6));  // Light red color
-
-  //     opposing_poly_marker.lifetime = rclcpp::Duration::from_seconds(0.3);
-
-  //     add_polygon_to_marker(opposing_poly_marker, polygon);
-
-  //     debug_marker_array.markers.push_back(opposing_poly_marker);
-  //   }
-  // }
+      debug_marker_array.markers.push_back(opposing_poly_marker);
+    }
+  }
 
   return debug_marker_array;
 }
