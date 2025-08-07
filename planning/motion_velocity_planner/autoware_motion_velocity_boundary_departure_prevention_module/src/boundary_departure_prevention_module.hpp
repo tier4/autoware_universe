22408ae00d79cd31e07b6c5ee2c2203bba1fee1f
Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
2025-08-07 12:52:10 +0900

    feat(boundary_departure): on time buffer for critical departure
    Signed-off-by: Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>

--- planning/motion_velocity_planner/autoware_motion_velocity_boundary_departure_prevention_module/src/boundary_departure_prevention_module.hpp
+++ planning/motion_velocity_planner/autoware_motion_velocity_boundary_departure_prevention_module/src/boundary_departure_prevention_module.hpp
@@ -60,8 +60,15 @@ private:
   tl::expected<VelocityPlanningResult, std::string> plan_slow_down_intervals(
     const TrajectoryPoints & raw_trajectory_points,
     const std::shared_ptr<const PlannerData> & planner_data);
+
+  void update_critical_departure_points(
+    const trajectory::Trajectory<TrajectoryPoint> & aw_ref_traj, const double offset_from_ego);
+
   std::unordered_map<DepartureType, bool> get_diagnostics(
     const double curr_vel, const double dist_with_offset_m);
+
+  bool is_departing_continuously();
+
   rclcpp::Clock::SharedPtr clock_ptr_;
 
   std::string module_name_;
@@ -83,6 +90,8 @@ private:
 
   std::unique_ptr<double> last_lost_time_ptr_;
   std::unique_ptr<double> last_found_time_ptr_;
+  std::unique_ptr<double> last_no_critical_dpt_time_ptr_;
+  std::unique_ptr<double> last_found_critical_dpt_time_ptr_;
 
   autoware_utils::InterProcessPollingSubscriber<Trajectory>::SharedPtr ego_pred_traj_polling_sub_;
   autoware_utils::InterProcessPollingSubscriber<Control>::SharedPtr control_cmd_polling_sub_;
