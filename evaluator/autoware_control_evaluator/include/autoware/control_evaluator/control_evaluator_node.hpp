6a9216d1d63170b5e5c6bb1fe019766e0faf8771
Takayuki Murooka <takayuki5168@gmail.com>
2025-09-03 00:12:22 +0900

    feat(control_evaluator): add longitudinal_velocity evaluation (#11094)
    * feat(control_evaluator): add longitudinal_velocity evaluation
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    fix
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    fix
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    * fix
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    
    ---------
    
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>
    Signed-off-by: xtk8532704 <1041084556@qq.com>

--- evaluator/autoware_control_evaluator/include/autoware/control_evaluator/control_evaluator_node.hpp
+++ evaluator/autoware_control_evaluator/include/autoware/control_evaluator/control_evaluator_node.hpp
@@ -57,6 +57,7 @@ using autoware_utils::Point2d;
 using autoware_vehicle_msgs::msg::SteeringReport;
 using geometry_msgs::msg::Point;
 using geometry_msgs::msg::Pose;
+using geometry_msgs::msg::Twist;
 using nav_msgs::msg::Odometry;
 using LaneletMapBin = autoware_map_msgs::msg::LaneletMapBin;
 using autoware_planning_msgs::msg::LaneletRoute;
@@ -90,6 +91,8 @@ public:
     const Odometry & odom, const AccelWithCovarianceStamped & accel_stamped);
   void AddSteeringMetricMsg(const SteeringReport & steering_report);
   void AddStopDeviationMetricMsg();
+  void AddVelocityDeviationMetricMsg(
+    const Trajectory & traj, const Pose & ego_pose, const Twist & twist);
   void onTimer();
 
 private:
@@ -128,29 +131,31 @@ private:
   double distance_filter_thr_m_;
 
   // Metric
-  const std::vector<Metric> metrics_ = {// collect all metrics
-                                        Metric::velocity,
-                                        Metric::acceleration,
-                                        Metric::jerk,
-                                        Metric::lateral_deviation,
-                                        Metric::lateral_deviation_abs,
-                                        Metric::yaw_deviation,
-                                        Metric::yaw_deviation_abs,
-                                        Metric::goal_longitudinal_deviation,
-                                        Metric::goal_longitudinal_deviation_abs,
-                                        Metric::goal_lateral_deviation,
-                                        Metric::goal_lateral_deviation_abs,
-                                        Metric::goal_yaw_deviation,
-                                        Metric::goal_yaw_deviation_abs,
-                                        Metric::left_boundary_distance,
-                                        Metric::right_boundary_distance,
-                                        Metric::steering_angle,
-                                        Metric::steering_angle_abs,
-                                        Metric::steering_rate,
-                                        Metric::steering_acceleration,
-                                        Metric::stop_deviation,
-                                        Metric::stop_deviation_abs,
-                                        Metric::closest_object_distance};
+  const std::vector<Metric> metrics_ = {
+    // collect all metrics
+    Metric::velocity,
+    Metric::acceleration,
+    Metric::jerk,
+    Metric::lateral_deviation,
+    Metric::lateral_deviation_abs,
+    Metric::yaw_deviation,
+    Metric::yaw_deviation_abs,
+    Metric::goal_longitudinal_deviation,
+    Metric::goal_longitudinal_deviation_abs,
+    Metric::goal_lateral_deviation,
+    Metric::goal_lateral_deviation_abs,
+    Metric::goal_yaw_deviation,
+    Metric::goal_yaw_deviation_abs,
+    Metric::left_boundary_distance,
+    Metric::right_boundary_distance,
+    Metric::steering_angle,
+    Metric::steering_angle_abs,
+    Metric::steering_rate,
+    Metric::steering_acceleration,
+    Metric::stop_deviation,
+    Metric::stop_deviation_abs,
+    Metric::closest_object_distance,
+    Metric::longitudinal_velocity_deviation};
 
   std::array<Accumulator<double>, static_cast<size_t>(Metric::SIZE)>
     metric_accumulators_;  // 3(min, max, mean) * metric_size
