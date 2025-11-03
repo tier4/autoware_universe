6c6eaecf612c2bbc47713a20936cc096345e5d16
mitukou1109 <mitukou1109@gmail.com>
2025-11-03 17:13:56 +0900

    use Trajectory instead of PathWithLaneId
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>

--- planning/behavior_velocity_planner/autoware_behavior_velocity_no_drivable_lane_module/src/experimental/scene.cpp
+++ planning/behavior_velocity_planner/autoware_behavior_velocity_no_drivable_lane_module/src/experimental/scene.cpp
@@ -14,6 +14,8 @@
 
 #include "scene.hpp"
 
+#include <autoware/trajectory/utils/find_nearest.hpp>
+#include <autoware/trajectory/utils/pretty_build.hpp>
 #include <autoware_utils/ros/marker_helper.hpp>
 #include <autoware_utils_geometry/geometry.hpp>
 
@@ -54,11 +56,11 @@ visualization_msgs::msg::MarkerArray createNoDrivableLaneMarkers(
     auto marker = create_default_marker(
       "map", now, "path_polygon intersection points", uid, Marker::POINTS,
       create_marker_scale(0.25, 0.25, 0.0), create_marker_color(1.0, 0.0, 0.0, 0.999));
-    const auto & p_first = debug_data.path_polygon_intersection.first_intersection_point;
+    const auto & p_first = debug_data.first_intersection_point;
     if (p_first) {
       marker.points.push_back(create_point(p_first->x, p_first->y, p_first->z));
     }
-    const auto & p_second = debug_data.path_polygon_intersection.second_intersection_point;
+    const auto & p_second = debug_data.second_intersection_point;
     if (p_second) {
       marker.points.push_back(create_point(p_second->x, p_second->y, p_second->z));
     }
@@ -86,31 +88,24 @@ NoDrivableLaneModule::NoDrivableLaneModule(
 }
 
 bool NoDrivableLaneModule::modifyPathVelocity(
-  Trajectory & path, const std::vector<geometry_msgs::msg::Point> & left_bound,
-  const std::vector<geometry_msgs::msg::Point> & right_bound, const PlannerData & planner_data)
+  Trajectory & path, [[maybe_unused]] const std::vector<geometry_msgs::msg::Point> & left_bound,
+  [[maybe_unused]] const std::vector<geometry_msgs::msg::Point> & right_bound,
+  const PlannerData & planner_data)
 {
-  auto path_msg = planning_utils::fromTrajectory(path, left_bound, right_bound);
-
   const auto & ego_pos = planner_data.current_odometry->pose.position;
   const auto & lanelet_map_ptr = planner_data.route_handler_->getLaneletMapPtr();
   const auto & no_drivable_lane = lanelet_map_ptr->laneletLayer.get(lane_id_);
   const auto & no_drivable_lane_polygon =
     lanelet::utils::to2D(no_drivable_lane).polygon2d().basicPolygon();
 
-  path_no_drivable_lane_polygon_intersection =
-    getPathIntersectionWithNoDrivableLanePolygon(path_msg, no_drivable_lane_polygon, ego_pos, 2);
+  const auto path_polygon_intersection =
+    no_drivable_lane::getPathIntersectionWithNoDrivableLanePolygon(
+      path, no_drivable_lane_polygon, 2);
 
-  distance_ego_first_intersection = 0.0;
+  const auto ego_front_s = autoware::experimental::trajectory::find_nearest_index(path, ego_pos) +
+                           planner_data.vehicle_info_.max_longitudinal_offset_m;
 
-  if (path_no_drivable_lane_polygon_intersection.first_intersection_point) {
-    first_intersection_point =
-      path_no_drivable_lane_polygon_intersection.first_intersection_point.value();
-    distance_ego_first_intersection = autoware::motion_utils::calcSignedArcLength(
-      path_msg.points, planner_data.current_odometry->pose.position, first_intersection_point);
-    distance_ego_first_intersection -= planner_data.vehicle_info_.max_longitudinal_offset_m;
-  }
-
-  initialize_debug_data(no_drivable_lane, ego_pos, planner_data);
+  initialize_debug_data(path, no_drivable_lane, ego_pos, path_polygon_intersection, planner_data);
 
   switch (state_) {
     case State::INIT: {
@@ -118,7 +113,7 @@ bool NoDrivableLaneModule::modifyPathVelocity(
         RCLCPP_INFO(logger_, "Init");
       }
 
-      handle_init_state();
+      handle_init_state(ego_front_s, path_polygon_intersection);
 
       break;
     }
@@ -128,7 +123,7 @@ bool NoDrivableLaneModule::modifyPathVelocity(
         RCLCPP_INFO(logger_, "Approaching ");
       }
 
-      handle_approaching_state(&path_msg, planner_data);
+      handle_approaching_state(path, ego_front_s, path_polygon_intersection, planner_data);
 
       break;
     }
@@ -138,7 +133,7 @@ bool NoDrivableLaneModule::modifyPathVelocity(
         RCLCPP_INFO(logger_, "INSIDE_NO_DRIVABLE_LANE");
       }
 
-      handle_inside_no_drivable_lane_state(&path_msg, planner_data);
+      handle_inside_no_drivable_lane_state(path, ego_front_s, planner_data);
 
       break;
     }
@@ -148,7 +143,7 @@ bool NoDrivableLaneModule::modifyPathVelocity(
         RCLCPP_INFO(logger_, "STOPPED");
       }
 
-      handle_stopped_state(&path_msg, planner_data);
+      handle_stopped_state(path, ego_front_s, planner_data);
 
       break;
     }
@@ -159,92 +154,59 @@ bool NoDrivableLaneModule::modifyPathVelocity(
     }
   }
 
-  planning_utils::toTrajectory(path_msg, path);
   return true;
 }
 
-void NoDrivableLaneModule::handle_init_state()
+void NoDrivableLaneModule::handle_init_state(
+  const double ego_front_s, const no_drivable_lane::PolygonIntersection & path_polygon_intersection)
 {
   if (
-    (path_no_drivable_lane_polygon_intersection.is_first_path_point_inside_polygon) ||
-    ((path_no_drivable_lane_polygon_intersection.first_intersection_point) &&
-     (distance_ego_first_intersection <= planner_param_.stop_margin))) {
+    path_polygon_intersection.is_first_path_point_inside_polygon ||
+    (path_polygon_intersection.first_intersection_s &&
+     *path_polygon_intersection.first_intersection_s - ego_front_s <= planner_param_.stop_margin)) {
     state_ = State::INSIDE_NO_DRIVABLE_LANE;
-  } else if (
-    (path_no_drivable_lane_polygon_intersection.first_intersection_point) &&
-    (distance_ego_first_intersection > planner_param_.stop_margin)) {
-    state_ = State::APPROACHING;
   } else {
-    state_ = State::INIT;
+    state_ = State::APPROACHING;
   }
 }
 
 void NoDrivableLaneModule::handle_approaching_state(
-  PathWithLaneId * path, const PlannerData & planner_data)
+  Trajectory & path, const double ego_front_s,
+  const no_drivable_lane::PolygonIntersection & path_polygon_intersection,
+  const PlannerData & planner_data)
 {
-  const double longitudinal_offset =
-    -1.0 * (planner_param_.stop_margin + planner_data.vehicle_info_.max_longitudinal_offset_m);
-
-  const auto op_target_point = autoware::motion_utils::calcLongitudinalOffsetPoint(
-    path->points, first_intersection_point, longitudinal_offset);
-
-  geometry_msgs::msg::Point target_point;
-
-  if (op_target_point) {
-    target_point = op_target_point.value();
+  if (!path_polygon_intersection.first_intersection_s) {
+    return;
   }
 
-  const auto target_segment_idx =
-    autoware::motion_utils::findNearestSegmentIndex(path->points, target_point);
+  const auto longitudinal_offset =
+    -(planner_param_.stop_margin + planner_data.vehicle_info_.max_longitudinal_offset_m);
+  const auto stop_s = *path_polygon_intersection.first_intersection_s + longitudinal_offset;
 
-  const auto & op_target_point_idx =
-    autoware::motion_utils::insertTargetPoint(target_segment_idx, target_point, path->points, 5e-2);
-  size_t target_point_idx = 0;
-  if (op_target_point_idx) {
-    target_point_idx = op_target_point_idx.value();
-  }
-
-  geometry_msgs::msg::Point stop_point =
-    autoware_utils_geometry::get_point(path->points.at(target_point_idx).point);
-
-  const auto & op_stop_pose =
-    planning_utils::insertStopPoint(stop_point, target_segment_idx, *path);
+  path.longitudinal_velocity_mps().range(stop_s, path.length()).set(0.0);
 
   // Get stop point and stop factor
-  {
-    const auto & stop_pose = op_stop_pose.value();
-    planning_factor_interface_->add(
-      path->points, planner_data.current_odometry->pose, stop_pose,
-      autoware_internal_planning_msgs::msg::PlanningFactor::STOP,
-      autoware_internal_planning_msgs::msg::SafetyFactorArray{}, true /*is_driving_forward*/, 0.0,
-      0.0 /*shift distance*/, "");
+  planning_factor_interface_->add(
+    path.restore(), planner_data.current_odometry->pose, path.compute(stop_s).point.pose,
+    autoware_internal_planning_msgs::msg::PlanningFactor::STOP,
+    autoware_internal_planning_msgs::msg::SafetyFactorArray{}, true /*is_driving_forward*/, 0.0,
+    0.0 /*shift distance*/, "");
 
-    const auto virtual_wall_pose = autoware::motion_utils::calcLongitudinalOffsetPose(
-      path->points, stop_pose.position, debug_data_.base_link2front);
-
-    debug_data_.stop_pose = virtual_wall_pose.value();
-  }
-
-  const size_t current_seg_idx = findEgoSegmentIndex(path->points, planner_data);
-  const auto intersection_segment_idx =
-    autoware::motion_utils::findNearestSegmentIndex(path->points, first_intersection_point);
-  const double signed_arc_dist_to_intersection_point =
-    autoware::motion_utils::calcSignedArcLength(
-      path->points, planner_data.current_odometry->pose.position, current_seg_idx,
-      first_intersection_point, intersection_segment_idx) -
-    planner_data.vehicle_info_.max_longitudinal_offset_m;
+  debug_data_.stop_pose = path.compute(stop_s + debug_data_.base_link2front).point.pose;
 
   // Move to stopped state if stopped
+  const auto distance_ego_first_intersection =
+    *path_polygon_intersection.first_intersection_s - ego_front_s;
   if (
-    (signed_arc_dist_to_intersection_point <= planner_param_.stop_margin) &&
-    (planner_data.isVehicleStopped())) {
+    (distance_ego_first_intersection <= planner_param_.stop_margin) &&
+    planner_data.isVehicleStopped()) {
     if (planner_param_.print_debug_info) {
       RCLCPP_INFO(logger_, "APPROACHING -> STOPPED");
       RCLCPP_INFO_STREAM(
-        logger_, "signed_arc_dist_to_stop_point = " << signed_arc_dist_to_intersection_point);
+        logger_, "distance_ego_first_intersection = " << distance_ego_first_intersection);
     }
 
-    if (signed_arc_dist_to_intersection_point < 0.0) {
+    if (distance_ego_first_intersection < 0.0) {
       RCLCPP_ERROR(
         logger_, "Failed to stop before no drivable lane but ego stopped. Change state to STOPPED");
     }
@@ -254,28 +216,21 @@ void NoDrivableLaneModule::handle_approaching_state(
 }
 
 void NoDrivableLaneModule::handle_inside_no_drivable_lane_state(
-  PathWithLaneId * path, const PlannerData & planner_data)
+  Trajectory & path, const double ego_front_s, const PlannerData & planner_data)
 {
-  const auto & current_point = planner_data.current_odometry->pose.position;
-  const size_t current_seg_idx = findEgoSegmentIndex(path->points, planner_data);
-
   // Insert stop point
-  planning_utils::insertStopPoint(current_point, current_seg_idx, *path);
+  path.longitudinal_velocity_mps()
+    .range(ego_front_s - planner_data.vehicle_info_.max_longitudinal_offset_m, path.length())
+    .set(0.0);
 
   // Get stop point and stop factor
-  {
-    const auto & stop_pose = autoware_utils_geometry::get_pose(path->points.at(0));
-    planning_factor_interface_->add(
-      path->points, planner_data.current_odometry->pose, stop_pose,
-      autoware_internal_planning_msgs::msg::PlanningFactor::STOP,
-      autoware_internal_planning_msgs::msg::SafetyFactorArray{}, true /*is_driving_forward*/, 0.0,
-      0.0 /*shift distance*/, "");
+  planning_factor_interface_->add(
+    path.restore(), planner_data.current_odometry->pose, path.compute(0).point.pose,
+    autoware_internal_planning_msgs::msg::PlanningFactor::STOP,
+    autoware_internal_planning_msgs::msg::SafetyFactorArray{}, true /*is_driving_forward*/, 0.0,
+    0.0 /*shift distance*/, "");
 
-    const auto & virtual_wall_pose = autoware::motion_utils::calcLongitudinalOffsetPose(
-      path->points, stop_pose.position, debug_data_.base_link2front);
-
-    debug_data_.stop_pose = virtual_wall_pose.value();
-  }
+  debug_data_.stop_pose = path.compute(debug_data_.base_link2front).point.pose;
 
   // Move to stopped state if stopped
   if (planner_data.isVehicleStopped()) {
@@ -287,46 +242,38 @@ void NoDrivableLaneModule::handle_inside_no_drivable_lane_state(
 }
 
 void NoDrivableLaneModule::handle_stopped_state(
-  PathWithLaneId * path, const PlannerData & planner_data)
+  Trajectory & path, const double ego_front_s, const PlannerData & planner_data)
 {
-  const auto & stopped_pose = autoware::motion_utils::calcLongitudinalOffsetPose(
-    path->points, planner_data.current_odometry->pose.position, 0.0);
-
-  if (!stopped_pose) {
-    state_ = State::INIT;
-    return;
-  }
-
-  SegmentIndexWithPose ego_pos_on_path;
-  ego_pos_on_path.pose = stopped_pose.value();
-  ego_pos_on_path.index = findEgoSegmentIndex(path->points, planner_data);
-
   // Insert stop pose
-  planning_utils::insertStopPoint(ego_pos_on_path.pose.position, ego_pos_on_path.index, *path);
+  const auto ego_s = ego_front_s - planner_data.vehicle_info_.max_longitudinal_offset_m;
+  path.longitudinal_velocity_mps().range(ego_s, path.length()).set(0.0);
 
   // Get stop point and stop factor
-  {
-    const auto & stop_pose = ego_pos_on_path.pose;
-    planning_factor_interface_->add(
-      path->points, planner_data.current_odometry->pose, stop_pose,
-      autoware_internal_planning_msgs::msg::PlanningFactor::STOP,
-      autoware_internal_planning_msgs::msg::SafetyFactorArray{}, true /*is_driving_forward*/, 0.0,
-      0.0 /*shift distance*/, "");
+  planning_factor_interface_->add(
+    path.restore(), planner_data.current_odometry->pose, path.compute(ego_s).point.pose,
+    autoware_internal_planning_msgs::msg::PlanningFactor::STOP,
+    autoware_internal_planning_msgs::msg::SafetyFactorArray{}, true /*is_driving_forward*/, 0.0,
+    0.0 /*shift distance*/, "");
 
-    const auto virtual_wall_pose = autoware::motion_utils::calcLongitudinalOffsetPose(
-      path->points, stop_pose.position, debug_data_.base_link2front);
-
-    debug_data_.stop_pose = virtual_wall_pose.value();
-  }
+  debug_data_.stop_pose = path.compute(ego_s + debug_data_.base_link2front).point.pose;
 }
 
 void NoDrivableLaneModule::initialize_debug_data(
-  const lanelet::Lanelet & no_drivable_lane, const geometry_msgs::msg::Point & ego_pos,
+  const Trajectory & path, const lanelet::Lanelet & no_drivable_lane,
+  const geometry_msgs::msg::Point & ego_pos,
+  const no_drivable_lane::PolygonIntersection & path_polygon_intersection,
   const PlannerData & planner_data)
 {
   debug_data_ = DebugData();
   debug_data_.base_link2front = planner_data.vehicle_info_.max_longitudinal_offset_m;
-  debug_data_.path_polygon_intersection = path_no_drivable_lane_polygon_intersection;
+  if (path_polygon_intersection.first_intersection_s) {
+    debug_data_.first_intersection_point =
+      path.compute(*path_polygon_intersection.first_intersection_s).point.pose.position;
+  }
+  if (path_polygon_intersection.second_intersection_s) {
+    debug_data_.second_intersection_point =
+      path.compute(*path_polygon_intersection.second_intersection_s).point.pose.position;
+  }
 
   for (const auto & p : no_drivable_lane.polygon2d().basicPolygon()) {
     debug_data_.no_drivable_lane_polygon.push_back(create_point(p.x(), p.y(), ego_pos.z));
