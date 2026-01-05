84811bba24e5a4052af87c30ba75c81507c44857
YuxuanLiuTier4Desktop <619684051@qq.com>
2026-01-05 16:47:05 +0900

    feat: directly copy the diffusion_planner and trajecotry optimizer from 3670a8b355514d452c2349dda508aff7aed944b3
    Signed-off-by: YuxuanLiuTier4Desktop <619684051@qq.com>

---
+++ planning/autoware_trajectory_optimizer/include/autoware/trajectory_optimizer/trajectory_optimizer_plugins/trajectory_velocity_optimizer.hpp
@@ -0,0 +1,74 @@
+// Copyright 2025 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// NOLINTNEXTLINE
+#ifndef AUTOWARE__TRAJECTORY_OPTIMIZER__TRAJECTORY_OPTIMIZER_PLUGINS__TRAJECTORY_VELOCITY_OPTIMIZER_HPP_
+// NOLINTNEXTLINE
+#define AUTOWARE__TRAJECTORY_OPTIMIZER__TRAJECTORY_OPTIMIZER_PLUGINS__TRAJECTORY_VELOCITY_OPTIMIZER_HPP_
+#include "autoware/trajectory_optimizer/trajectory_optimizer_plugins/trajectory_optimizer_plugin_base.hpp"
+#include "autoware/velocity_smoother/smoother/jerk_filtered_smoother.hpp"
+
+#include <autoware_utils/system/time_keeper.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+#include <autoware_planning_msgs/msg/trajectory.hpp>
+#include <autoware_planning_msgs/msg/trajectory_point.hpp>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace autoware::trajectory_optimizer::plugin
+{
+using autoware_planning_msgs::msg::TrajectoryPoint;
+using TrajectoryPoints = std::vector<TrajectoryPoint>;
+using autoware::velocity_smoother::JerkFilteredSmoother;
+
+struct TrajectoryVelocityOptimizerParams
+{
+  double nearest_dist_threshold_m{1.5};
+  double nearest_yaw_threshold_deg{60.0};
+  double target_pull_out_speed_mps{1.0};
+  double target_pull_out_acc_mps2{1.0};
+  double max_speed_mps{8.33};
+  double max_lateral_accel_mps2{1.5};
+  bool set_engage_speed{false};
+  bool limit_speed{true};
+  bool limit_lateral_acceleration{false};
+  bool smooth_velocities{false};
+};
+
+class TrajectoryVelocityOptimizer : public TrajectoryOptimizerPluginBase
+{
+public:
+  TrajectoryVelocityOptimizer() = default;
+  ~TrajectoryVelocityOptimizer() = default;
+
+  void set_up_velocity_smoother(
+    rclcpp::Node * node_ptr, const std::shared_ptr<autoware_utils_debug::TimeKeeper> time_keeper);
+  void optimize_trajectory(
+    TrajectoryPoints & traj_points, const TrajectoryOptimizerParams & params,
+    const TrajectoryOptimizerData & data) override;
+  void set_up_params() override;
+  rcl_interfaces::msg::SetParametersResult on_parameter(
+    const std::vector<rclcpp::Parameter> & parameters) override;
+
+private:
+  std::shared_ptr<JerkFilteredSmoother> jerk_filtered_smoother_{nullptr};
+  TrajectoryVelocityOptimizerParams velocity_params_;
+};
+}  // namespace autoware::trajectory_optimizer::plugin
+
+// NOLINTNEXTLINE
+#endif  // AUTOWARE__TRAJECTORY_OPTIMIZER__TRAJECTORY_OPTIMIZER_PLUGINS__TRAJECTORY_VELOCITY_OPTIMIZER_HPP_
---
+++ planning/autoware_trajectory_optimizer/src/trajectory_optimizer_plugins/trajectory_velocity_optimizer.cpp
@@ -0,0 +1,162 @@
+// Copyright 2025 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "autoware/trajectory_optimizer/trajectory_optimizer_plugins/trajectory_velocity_optimizer.hpp"
+
+#include "autoware/trajectory_optimizer/trajectory_optimizer_plugins/plugin_utils/trajectory_velocity_optimizer_utils.hpp"
+
+#include <autoware_utils_math/unit_conversion.hpp>
+#include <autoware_utils_rclcpp/parameter.hpp>
+#include <autoware_vehicle_info_utils/vehicle_info_utils.hpp>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace autoware::trajectory_optimizer::plugin
+{
+
+void TrajectoryVelocityOptimizer::set_up_velocity_smoother(
+  rclcpp::Node * node_ptr, const std::shared_ptr<autoware_utils_debug::TimeKeeper> time_keeper)
+{
+  const auto vehicle_info =
+    autoware::vehicle_info_utils::VehicleInfoUtils(*node_ptr).getVehicleInfo();
+  double wheelbase = vehicle_info.wheel_base_m;  // vehicle_info.wheel_base_m;
+  jerk_filtered_smoother_ = std::make_shared<JerkFilteredSmoother>(*node_ptr, time_keeper);
+  jerk_filtered_smoother_->setWheelBase(wheelbase);
+}
+
+void TrajectoryVelocityOptimizer::optimize_trajectory(
+  TrajectoryPoints & traj_points, const TrajectoryOptimizerParams & params,
+  const TrajectoryOptimizerData & data)
+{
+  if (!params.use_velocity_optimizer) {
+    return;
+  }
+
+  const auto & current_odometry = data.current_odometry;
+  const auto & current_acceleration = data.current_acceleration;
+  const auto & current_speed = current_odometry.twist.twist.linear.x;
+  const auto & current_linear_acceleration = current_acceleration.accel.accel.linear.x;
+  const double & target_pull_out_speed_mps = velocity_params_.target_pull_out_speed_mps;
+  const double & target_pull_out_acc_mps2 = velocity_params_.target_pull_out_acc_mps2;
+  const double & max_speed_mps = velocity_params_.max_speed_mps;
+
+  if (velocity_params_.limit_lateral_acceleration) {
+    trajectory_velocity_optimizer_utils::limit_lateral_acceleration(
+      traj_points, velocity_params_.max_lateral_accel_mps2, data.current_odometry);
+  }
+
+  auto initial_motion_speed =
+    (current_speed > target_pull_out_speed_mps) ? current_speed : target_pull_out_speed_mps;
+  auto initial_motion_acc = (current_speed > target_pull_out_speed_mps)
+                              ? current_linear_acceleration
+                              : target_pull_out_acc_mps2;
+
+  if (velocity_params_.set_engage_speed && (current_speed < target_pull_out_speed_mps)) {
+    trajectory_velocity_optimizer_utils::clamp_velocities(
+      traj_points, static_cast<float>(initial_motion_speed),
+      static_cast<float>(initial_motion_acc));
+  }
+
+  if (velocity_params_.limit_speed) {
+    trajectory_velocity_optimizer_utils::set_max_velocity(
+      traj_points, static_cast<float>(max_speed_mps));
+  }
+
+  if (velocity_params_.smooth_velocities) {
+    if (!jerk_filtered_smoother_) {
+      set_up_velocity_smoother(get_node_ptr(), get_time_keeper());
+    }
+    InitialMotion initial_motion{initial_motion_speed, initial_motion_acc};
+    trajectory_velocity_optimizer_utils::filter_velocity(
+      traj_points, initial_motion, velocity_params_.nearest_dist_threshold_m,
+      autoware_utils_math::deg2rad(velocity_params_.nearest_yaw_threshold_deg),
+      jerk_filtered_smoother_, current_odometry);
+  }
+}
+
+void TrajectoryVelocityOptimizer::set_up_params()
+{
+  auto node_ptr = get_node_ptr();
+  using autoware_utils_rclcpp::get_or_declare_parameter;
+
+  velocity_params_.nearest_dist_threshold_m = get_or_declare_parameter<double>(
+    *node_ptr, "trajectory_velocity_optimizer.nearest_dist_threshold_m");
+  velocity_params_.nearest_yaw_threshold_deg = get_or_declare_parameter<double>(
+    *node_ptr, "trajectory_velocity_optimizer.nearest_yaw_threshold_deg");
+  velocity_params_.target_pull_out_speed_mps = get_or_declare_parameter<double>(
+    *node_ptr, "trajectory_velocity_optimizer.target_pull_out_speed_mps");
+  velocity_params_.target_pull_out_acc_mps2 = get_or_declare_parameter<double>(
+    *node_ptr, "trajectory_velocity_optimizer.target_pull_out_acc_mps2");
+  velocity_params_.max_speed_mps =
+    get_or_declare_parameter<double>(*node_ptr, "trajectory_velocity_optimizer.max_speed_mps");
+  velocity_params_.max_lateral_accel_mps2 = get_or_declare_parameter<double>(
+    *node_ptr, "trajectory_velocity_optimizer.max_lateral_accel_mps2");
+  velocity_params_.set_engage_speed =
+    get_or_declare_parameter<bool>(*node_ptr, "trajectory_velocity_optimizer.set_engage_speed");
+  velocity_params_.limit_speed =
+    get_or_declare_parameter<bool>(*node_ptr, "trajectory_velocity_optimizer.limit_speed");
+  velocity_params_.limit_lateral_acceleration = get_or_declare_parameter<bool>(
+    *node_ptr, "trajectory_velocity_optimizer.limit_lateral_acceleration");
+  velocity_params_.smooth_velocities =
+    get_or_declare_parameter<bool>(*node_ptr, "trajectory_velocity_optimizer.smooth_velocities");
+}
+
+rcl_interfaces::msg::SetParametersResult TrajectoryVelocityOptimizer::on_parameter(
+  const std::vector<rclcpp::Parameter> & parameters)
+{
+  using autoware_utils_rclcpp::update_param;
+
+  update_param(
+    parameters, "trajectory_velocity_optimizer.nearest_dist_threshold_m",
+    velocity_params_.nearest_dist_threshold_m);
+  update_param(
+    parameters, "trajectory_velocity_optimizer.nearest_yaw_threshold_deg",
+    velocity_params_.nearest_yaw_threshold_deg);
+  update_param(
+    parameters, "trajectory_velocity_optimizer.target_pull_out_speed_mps",
+    velocity_params_.target_pull_out_speed_mps);
+  update_param(
+    parameters, "trajectory_velocity_optimizer.target_pull_out_acc_mps2",
+    velocity_params_.target_pull_out_acc_mps2);
+  update_param(
+    parameters, "trajectory_velocity_optimizer.max_speed_mps", velocity_params_.max_speed_mps);
+  update_param(
+    parameters, "trajectory_velocity_optimizer.max_lateral_accel_mps2",
+    velocity_params_.max_lateral_accel_mps2);
+  update_param(
+    parameters, "trajectory_velocity_optimizer.set_engage_speed",
+    velocity_params_.set_engage_speed);
+  update_param(
+    parameters, "trajectory_velocity_optimizer.limit_speed", velocity_params_.limit_speed);
+  update_param(
+    parameters, "trajectory_velocity_optimizer.limit_lateral_acceleration",
+    velocity_params_.limit_lateral_acceleration);
+  update_param(
+    parameters, "trajectory_velocity_optimizer.smooth_velocities",
+    velocity_params_.smooth_velocities);
+
+  rcl_interfaces::msg::SetParametersResult result;
+  result.successful = true;
+  result.reason = "success";
+  return result;
+}
+
+}  // namespace autoware::trajectory_optimizer::plugin
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(
+  autoware::trajectory_optimizer::plugin::TrajectoryVelocityOptimizer,
+  autoware::trajectory_optimizer::plugin::TrajectoryOptimizerPluginBase)
