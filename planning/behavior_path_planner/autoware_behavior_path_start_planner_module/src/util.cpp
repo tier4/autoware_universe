0317a96bb5811bd2069523639fdaa95f516ce304
Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
2026-01-22 16:51:37 +0900

    fix(start_planner): computing shift length when creating path
    Signed-off-by: Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_start_planner_module/src/util.cpp
+++ planning/behavior_path_planner/autoware_behavior_path_start_planner_module/src/util.cpp
@@ -268,4 +268,12 @@ void set_lane_ids_to_path_point(
   point.lane_ids = get_lane_ids_from_pose(point.point.pose, road_lanes, previous_lane_ids);
 }
 
+std::pair<double, double> calc_start_and_end_shift_length(
+  const lanelet::ConstLanelets & pull_out_lanes, const Pose & start_pose, const Pose & end_pose)
+{
+  double start_shift_length =
+    lanelet::utils::getArcCoordinates(pull_out_lanes, start_pose).distance;
+  double finish_shift_length = lanelet::utils::getArcCoordinates(pull_out_lanes, end_pose).distance;
+  return {start_shift_length, finish_shift_length};
+}
 }  // namespace autoware::behavior_path_planner::start_planner_utils
