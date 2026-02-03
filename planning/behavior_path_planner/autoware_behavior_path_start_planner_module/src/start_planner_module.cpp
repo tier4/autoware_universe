e7aa71cdd56300722bb3e45d15971d39ac39aa4d
Maxime CLEMENT <maxime.clement@tier4.jp>
2026-02-03 10:18:12 +0900

    add explicit stop point insertion
    Signed-off-by: Maxime CLEMENT <maxime.clement@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_start_planner_module/src/start_planner_module.cpp
+++ planning/behavior_path_planner/autoware_behavior_path_start_planner_module/src/start_planner_module.cpp
@@ -1022,6 +1022,10 @@ BehaviorModuleOutput StartPlannerModule::planWaitingApproval()
     previous_stop_pose_ = stop_pose_ = utils::insert_feasible_stop_point(
       stop_path, planner_data_, -parameters_->maximum_deceleration_for_stop,
       parameters_->maximum_jerk_for_stop, stop_reason);
+  } else {
+    const auto stop_length =
+      motion_utils::calcSignedArcLength(stop_path.points, 0UL, stop_pose_->pose.position);
+    utils::insertStopPoint(stop_length, stop_path);
   }
 
   const auto drivable_lanes = generateDrivableLanes(stop_path);
