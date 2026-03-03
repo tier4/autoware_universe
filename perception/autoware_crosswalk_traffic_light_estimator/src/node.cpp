1b0d757afe9707c4bc399337aa3100be151081a9
nishikawa-masaki <masaki.nishikawa@tier4.jp>
2026-03-03 09:51:36 +0900

    perf(perception): use emplace_back and emplace to avoid temporary object creation (#12201)
    * perf(perception): use emplace_back to avoid temporary object creation
    
    Signed-off-by: nishikawa-masaki <masaki.nishikawa@tier4.jp>
    
    * style(pre-commit): autofix
    
    * perf(perception): use emplace/emplace_back for most containers
    
    Signed-off-by: nishikawa-masaki <masaki.nishikawa@tier4.jp>
    
    * style(pre-commit): autofix
    
    ---------
    
    Signed-off-by: nishikawa-masaki <masaki.nishikawa@tier4.jp>
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>
    Co-authored-by: Taekjin LEE <taekjin.lee@tier4.jp>

--- perception/autoware_crosswalk_traffic_light_estimator/src/node.cpp
+++ perception/autoware_crosswalk_traffic_light_estimator/src/node.cpp
@@ -326,7 +326,7 @@ void CrosswalkTrafficLightEstimatorNode::updateLastDetectedSignal(
     const auto & id = input_traffic_signal.second.first.traffic_light_group_id;
 
     if (last_detect_color_.count(id) == 0) {
-      last_detect_color_.insert(std::make_pair(id, input_traffic_signal.second));
+      last_detect_color_.emplace(id, input_traffic_signal.second);
       continue;
     }
 
@@ -372,7 +372,7 @@ void CrosswalkTrafficLightEstimatorNode::updateLastDetectedSignals(
 
     if (last_colors_.count(id) == 0) {
       std::vector<TrafficSignalAndTime> signal{input_traffic_signal.second};
-      last_colors_.insert(std::make_pair(id, signal));
+      last_colors_.emplace(id, signal);
       continue;
     }
 
@@ -505,7 +505,7 @@ void CrosswalkTrafficLightEstimatorNode::updateFlashingState(const TrafficSignal
 
   // no record of detected color in last_detect_color_hold_time_
   if (is_flashing_.count(id) == 0) {
-    is_flashing_.insert(std::make_pair(id, false));
+    is_flashing_.emplace(id, false);
     return;
   }
 
@@ -542,7 +542,7 @@ uint8_t CrosswalkTrafficLightEstimatorNode::updateAndGetColorState(const Traffic
   const auto color = signal.elements[0].color;
 
   if (current_color_state_.count(id) == 0) {
-    current_color_state_.insert(std::make_pair(id, color));
+    current_color_state_.emplace(id, color);
   } else if (is_flashing_.at(id) == false) {
     current_color_state_.at(id) = color;
   } else if (is_flashing_.at(id) == true) {
