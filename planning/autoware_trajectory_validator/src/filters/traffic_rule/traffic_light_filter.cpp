41ee3d1fde5a8dd66ee5dc78a574938350893125
Maxime CLEMENT <78338830+maxime-clem@users.noreply.github.com>
2026-03-26 11:18:05 +0900

    feat(traffic_light_filter): add maximum checked trajectory length (#2802)
    Signed-off-by: Maxime CLEMENT <maxime.clement@tier4.jp>

--- planning/autoware_trajectory_validator/src/filters/traffic_rule/traffic_light_filter.cpp
+++ planning/autoware_trajectory_validator/src/filters/traffic_rule/traffic_light_filter.cpp
@@ -26,6 +26,7 @@
 
 #include <lanelet2_core/LaneletMap.h>
 #include <lanelet2_core/geometry/Lanelet.h>
+#include <lanelet2_core/geometry/Point.h>
 #include <lanelet2_core/primitives/BasicRegulatoryElements.h>
 #include <lanelet2_core/primitives/BoundingBox.h>
 #include <lanelet2_core/primitives/LineString.h>
@@ -128,17 +129,28 @@ tl::expected<void, std::string> TrafficLightFilter::is_feasible(
   }
   TrajectoryPoints trajectory;
   lanelet::BasicLineString2d trajectory_ls;
+  constexpr auto delay_response_time = 0.0;
+  const auto distance_for_ego_to_stop = motion_utils::calculate_stop_distance(
+    context.odometry->twist.twist.linear.x, context.acceleration->accel.accel.linear.x,
+    params_.checked_trajectory_length.deceleration_limit,
+    params_.checked_trajectory_length.jerk_limit, delay_response_time);
+  const auto max_trajectory_length = distance_for_ego_to_stop.value_or(0.0);
+  auto length = 0.0;
   for (const auto & p : traj_points) {
     // skip points behind ego
     if (rclcpp::Duration(p.time_from_start).seconds() < 0.0) {
       continue;
     }
-    // skip points beyond the first stop
-    if (p.longitudinal_velocity_mps <= 0.0) {
+    const lanelet::BasicPoint2d lanelet_p(p.pose.position.x, p.pose.position.y);
+    if (!trajectory_ls.empty()) {
+      length += lanelet::geometry::distance2d(trajectory_ls.back(), lanelet_p);
+    }
+    // skip points beyond the first stop, or skip once we reach the maximum length
+    if (p.longitudinal_velocity_mps <= 0.0 || length > max_trajectory_length) {
       break;
     }
     trajectory.push_back(p);
-    trajectory_ls.emplace_back(p.pose.position.x, p.pose.position.y);
+    trajectory_ls.emplace_back(lanelet_p);
   }
 
   if (trajectory_ls.size() < 2) {
