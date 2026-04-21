684addbaf204863b1fccee254cac814890e25a6d
t4-adc <grp-rd-1-adc-admin@tier4.jp>
2026-04-21 11:24:39 +0900

    feat(control_evaluator): add
    Signed-off-by: t4-adc <grp-rd-1-adc-admin@tier4.jp>
    
    tmp
    
    Signed-off-by: t4-adc <grp-rd-1-adc-admin@tier4.jp>
    
    fix pre-commit
    
    Signed-off-by: t4-adc <grp-rd-1-adc-admin@tier4.jp>
    
    archive
    
    Signed-off-by: t4-adc <grp-rd-1-adc-admin@tier4.jp>
    
    fix for archive
    
    Signed-off-by: t4-adc <grp-rd-1-adc-admin@tier4.jp>

--- evaluator/autoware_control_evaluator/src/metrics/metrics_utils.cpp
+++ evaluator/autoware_control_evaluator/src/metrics/metrics_utils.cpp
@@ -16,13 +16,9 @@
 
 #include "autoware/motion_utils/trajectory/trajectory.hpp"
 
-#include <autoware_lanelet2_extension/utility/message_conversion.hpp>
-#include <autoware_lanelet2_extension/utility/query.hpp>
-#include <autoware_lanelet2_extension/utility/utilities.hpp>
 #include <autoware_utils/geometry/boost_geometry.hpp>
 
 #include <boost/geometry.hpp>
-// #include <boost/geometry/algorithms/detail/distance/interface.hpp>
 
 #include <lanelet2_core/Forward.h>
 
