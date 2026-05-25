507e82cc49d8d07c90c830e39e6c6b0320782595
Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
2026-05-25 14:19:07 +0900

    T4DEV-53800 fix: use timer based publishing
    Signed-off-by: Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>

--- perception/autoware_proximity_hazard_object_checker/include/autoware/proximity_hazard_object_checker/proximity_hazard_object_checker_node.hpp
+++ perception/autoware_proximity_hazard_object_checker/include/autoware/proximity_hazard_object_checker/proximity_hazard_object_checker_node.hpp
@@ -9,9 +9,11 @@
 #include "autoware/proximity_hazard_object_checker/proximity_hazard_object_checker.hpp"
 
 #include <autoware_proximity_hazard_object_checker/msg/proximity_hazard_objects.hpp>
+#include <autoware_utils_rclcpp/polling_subscriber.hpp>
 #include <rclcpp/rclcpp.hpp>
 
 #include <autoware_perception_msgs/msg/predicted_objects.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
@@ -20,9 +22,9 @@
 
 namespace autoware::proximity_hazard_object_checker
 {
-
 using autoware_perception_msgs::msg::PredictedObjects;
 using autoware_proximity_hazard_object_checker::msg::ProximityHazardObjects;
+using nav_msgs::msg::Odometry;
 
 class ProximityHazardObjectCheckerNode : public rclcpp::Node
 {
@@ -30,20 +32,36 @@ public:
   explicit ProximityHazardObjectCheckerNode(const rclcpp::NodeOptions & options);
 
 private:
-  void on_objects(PredictedObjects::ConstSharedPtr msg);
+  void on_timer();
+
+  template <typename... Args>
+
+  void error_throttle(const char * fmt)
+  {
+    constexpr int throttle_ms = 5000;
+    RCLCPP_ERROR_THROTTLE(get_logger(), *get_clock(), throttle_ms, fmt);
+  }
+
+  template <typename... Args>
+  void error_throttle(const char * fmt, Args... args)
+  {
+    constexpr int throttle_ms = 5000;
+    RCLCPP_ERROR_THROTTLE(get_logger(), *get_clock(), throttle_ms, fmt, args...);
+  }
 
   // I/O
-  rclcpp::Subscription<PredictedObjects>::SharedPtr sub_objects_;
+  autoware_utils_rclcpp::InterProcessPollingSubscriber<Odometry> sub_odometry_{
+    this, "~/input/odometry"};
+  autoware_utils_rclcpp::InterProcessPollingSubscriber<PredictedObjects> sub_objects_{
+    this, "~/input/objects"};
   rclcpp::Publisher<ProximityHazardObjects>::SharedPtr pub_hazards_;
+  rclcpp::TimerBase::SharedPtr timer_;
 
-  // TF
   std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
   std::unique_ptr<tf2_ros::TransformListener> tf_listener_;
 
-  // Parameter listener (generate_parameter_library).
   std::shared_ptr<proximity_hazard_object::ParamListener> param_listener_;
 
-  // The actual logic.
   std::unique_ptr<ProximityHazardObjectChecker> impl_;
 };
 
--- perception/autoware_proximity_hazard_object_checker/src/proximity_hazard_object_checker_node.cpp
+++ perception/autoware_proximity_hazard_object_checker/src/proximity_hazard_object_checker_node.cpp
@@ -12,20 +12,17 @@
 
 namespace autoware::proximity_hazard_object_checker
 {
-
-namespace
-{
-// Vehicle reference frame for TF lookup and internal geometry. The output header's
-// frame_id is propagated from the input PredictedObjects, not this frame.
-constexpr auto kBaseFrame = "base_link";
-}  // namespace
-
 ProximityHazardObjectCheckerNode::ProximityHazardObjectCheckerNode(
   const rclcpp::NodeOptions & options)
 : rclcpp::Node("proximity_hazard_object_checker", options)
 {
   param_listener_ =
     std::make_shared<proximity_hazard_object::ParamListener>(get_node_parameters_interface());
+
+  const auto node_ms = 1 / param_listener_->get_params().node_hz;
+  timer_ = rclcpp::create_timer(
+    this, get_clock(), rclcpp::Duration::from_seconds(node_ms),
+    std::bind(&ProximityHazardObjectCheckerNode::on_timer, this));
 
   const auto vehicle_info = autoware::vehicle_info_utils::VehicleInfoUtils(*this).getVehicleInfo();
 
@@ -35,28 +32,38 @@ ProximityHazardObjectCheckerNode::ProximityHazardObjectCheckerNode(
   tf_buffer_ = std::make_unique<tf2_ros::Buffer>(get_clock());
   tf_listener_ = std::make_unique<tf2_ros::TransformListener>(*tf_buffer_);
 
-  sub_objects_ = create_subscription<PredictedObjects>(
-    "~/input/objects", rclcpp::QoS{1},
-    std::bind(&ProximityHazardObjectCheckerNode::on_objects, this, std::placeholders::_1));
-
   pub_hazards_ = create_publisher<ProximityHazardObjects>(
     "~/output/proximity_hazards", rclcpp::QoS{1}.reliable());
 }
 
-void ProximityHazardObjectCheckerNode::on_objects(PredictedObjects::ConstSharedPtr msg)
+void ProximityHazardObjectCheckerNode::on_timer()
 {
-  geometry_msgs::msg::TransformStamped to_base_link;
-  try {
-    to_base_link = tf_buffer_->lookupTransform(
-      kBaseFrame, msg->header.frame_id, msg->header.stamp, rclcpp::Duration::from_seconds(0.1));
-  } catch (const tf2::TransformException & ex) {
+  const auto object_ptr = sub_objects_.take_data();
+
+  if (!object_ptr) {
+    RCLCPP_WARN_THROTTLE(get_logger(), *get_clock(), 5000, "Failed to take predicted objects data");
+    return;
+  }
+  const auto odometry_ptr = sub_odometry_.take_data();
+  if (!object_ptr) {
     RCLCPP_WARN_THROTTLE(
-      get_logger(), *get_clock(), 1000, "TF lookup %s -> %s failed: %s",
-      msg->header.frame_id.c_str(), kBaseFrame, ex.what());
+      get_logger(), *get_clock(), 5000, "Failed to take predicted odometry data");
     return;
   }
 
-  pub_hazards_->publish(impl_->process(*msg, to_base_link));
+  geometry_msgs::msg::TransformStamped to_base_link;
+  try {
+    to_base_link = tf_buffer_->lookupTransform(
+      odometry_ptr->child_frame_id, object_ptr->header.frame_id, object_ptr->header.stamp,
+      rclcpp::Duration::from_seconds(0.1));
+  } catch (const tf2::TransformException & ex) {
+    RCLCPP_WARN_THROTTLE(
+      get_logger(), *get_clock(), 5000, "TF lookup %s -> %s failed: %s",
+      object_ptr->header.frame_id.c_str(), odometry_ptr->child_frame_id.c_str(), ex.what());
+    return;
+  }
+
+  pub_hazards_->publish(impl_->process(*object_ptr, to_base_link));
 }
 
 }  // namespace autoware::proximity_hazard_object_checker
