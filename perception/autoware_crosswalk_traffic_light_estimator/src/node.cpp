5416a25350180c2f8446473251b5c9126dc25c11
Akifumi Ohata <akifumi.ohata@tier4.jp>
2026-06-22 13:58:47 +0900

    precommit

--- perception/autoware_crosswalk_traffic_light_estimator/src/node.cpp
+++ perception/autoware_crosswalk_traffic_light_estimator/src/node.cpp
@@ -503,7 +503,6 @@ void CrosswalkTrafficLightEstimatorNode::update_intersection_overrides_from_map(
   }
 
   for (const auto & [id, color] : traffic_signal_overrides) {
-
     if (existing_ids.count(id)) {
       continue;
     }
@@ -511,7 +510,7 @@ void CrosswalkTrafficLightEstimatorNode::update_intersection_overrides_from_map(
     TrafficSignalElement element;
     element.color = color;
     element.shape = TrafficSignalElement::CIRCLE;
-    element.confidence = 1.0; 
+    element.confidence = 1.0;
 
     TrafficSignal new_signal;
     new_signal.traffic_light_group_id = id;
