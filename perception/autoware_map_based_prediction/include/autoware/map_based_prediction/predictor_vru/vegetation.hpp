a1763e6148b45ecbea747f29cc374facc5d4c9fe
ohtt <mojanekonihiki@gmail.com>
2026-06-29 15:02:50 +0900

    init

---
+++ perception/autoware_map_based_prediction/include/autoware/map_based_prediction/predictor_vru/vegetation.hpp
@@ -0,0 +1,45 @@
+// Copyright 2026 TIER IV, inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef AUTOWARE__MAP_BASED_PREDICTION__PREDICTOR_VRU__VEGETATION_HPP_
+#define AUTOWARE__MAP_BASED_PREDICTION__PREDICTOR_VRU__VEGETATION_HPP_
+
+#include "autoware/map_based_prediction/path_generator/path_generator.hpp"
+
+#include <lanelet2_core/LaneletMap.h>
+
+#include <memory>
+
+namespace autoware::map_based_prediction
+{
+
+class VegetationModule
+{
+public:
+  VegetationModule() = default;
+
+  void buildFromMap(std::shared_ptr<lanelet::LaneletMap> lanelet_map_ptr);
+
+  [[nodiscard]] bool doesPathCrossAnyVegetationBeforeCrosswalk(
+    const PredictedPathWithArrivalIndex & predicted_path) const;
+
+  [[nodiscard]] PredictedPath cutPathBeforeVegetation(const PredictedPath & predicted_path) const;
+
+private:
+  lanelet::LaneletMapUPtr vegetation_layer_{nullptr};
+};
+
+}  // namespace autoware::map_based_prediction
+
+#endif  // AUTOWARE__MAP_BASED_PREDICTION__PREDICTOR_VRU__VEGETATION_HPP_
