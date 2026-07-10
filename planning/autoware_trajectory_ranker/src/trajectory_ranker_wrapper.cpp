7b8fd65beeeb2da1dd6a7ebc4537d5566613b3e2
Mohammad <alqudah.mohammad@tier4.jp>
2026-07-10 16:16:37 +0900

    add trajectory_ranker_wrapper framework
    Signed-off-by: Mohammad <alqudah.mohammad@tier4.jp>

---
+++ planning/autoware_trajectory_ranker/src/trajectory_ranker_wrapper.cpp
@@ -0,0 +1,60 @@
+// Copyright 2026 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <autoware/trajectory_ranker/trajectory_ranker_wrapper.hpp>
+
+#include <memory>
+#include <utility>
+
+namespace autoware::trajectory_ranker
+{
+
+TrajectoryRankerWrapper::TrajectoryRankerWrapper(
+  rclcpp::Node & node,
+  rclcpp::node_interfaces::NodeParametersInterface::SharedPtr node_parameters_interface,
+  vehicle_info_utils::VehicleInfo vehicle_info,
+  std::shared_ptr<autoware_utils_debug::TimeKeeper> time_keeper)
+: node_ptr_(&node),
+  logger_(node.get_logger().get_child(interface_name_)),
+  vehicle_info_(vehicle_info),
+  time_keeper_(std::move(time_keeper)),
+  param_listener_(
+    std::make_unique<trajectory_ranker_params::ParamListener>(node_parameters_interface))
+{
+  if (!time_keeper_) {
+    throw std::runtime_error("TimeKeeper is required for TrajectoryRankerWrapper");
+  }
+
+  params_ = param_listener_->get_params();
+}
+
+void TrajectoryRankerWrapper::update_parameters()
+{
+  if (param_listener_->is_old(params_)) {
+    params_ = param_listener_->get_params();
+    RCLCPP_INFO(logger_, "Trajectory Ranker parameters are updated.");
+  }
+}
+
+ScoredCandidateTrajectories TrajectoryRankerWrapper::rank_trajectories(
+  const CandidateTrajectories & input_trajectories)
+{
+  update_parameters();
+
+  (void)input_trajectories;
+
+  return ScoredCandidateTrajectories();
+}
+
+}  // namespace autoware::trajectory_ranker
