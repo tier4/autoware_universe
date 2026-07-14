698f4e92d93716fd99e1e771795ea7fa252b401c
Arjun Jagdish Ram <arjun.ram@tier4.jp>
2026-07-14 18:23:41 +0900

    Added boundary violation check
    Signed-off-by: Arjun Jagdish Ram <arjun.ram@tier4.jp>

--- planning/autoware_diffusion_planner/include/autoware/diffusion_planner/diffusion_planner_node.hpp
+++ planning/autoware_diffusion_planner/include/autoware/diffusion_planner/diffusion_planner_node.hpp
@@ -186,7 +186,11 @@ private:
     const autoware::mppi_optimizer::FirstOrderDubinsMppiDebug & debug, const std::string & frame_id,
     const rclcpp::Time & stamp);
 
-  void publish_avoidance_targets(
+  /**
+   * Publish avoidance / driving-along targets and drivable-area bounds.
+   * @return Drivable area Path (left/right bounds) when detection succeeds, else nullopt.
+   */
+  std::optional<Path> publish_avoidance_targets(
     const rclcpp::Time & stamp, const Trajectory & trajectory,
     const PredictedObjects & predicted_objects,
     const std::shared_ptr<const TrackedObjects> & tracked_objects);
