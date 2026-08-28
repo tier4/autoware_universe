6d652a5bda07686010a9adecd8cd56d3d1ba2eec
Maxime CLEMENT <maxime.clement@tier4.jp>
2026-08-28 20:40:56 +0900

    add debug visualization
    Signed-off-by: Maxime CLEMENT <maxime.clement@tier4.jp>

--- planning/autoware_mppi_optimizer/include/autoware/mppi_optimizer/first_order_dubins_mppi_runtime_options.hpp
+++ planning/autoware_mppi_optimizer/include/autoware/mppi_optimizer/first_order_dubins_mppi_runtime_options.hpp
@@ -26,6 +26,8 @@ struct FirstOrderDubinsMppiRuntimeOptions
   bool enable_debug_trajectory_log{false};
   /** Empty -> process current working directory. */
   std::string debug_trajectory_log_directory;
+  /** Open the zero-copy CUDA-OpenGL distance-map texture debug window. */
+  bool enable_distance_map_texture_debug{false};
   bool ignore_obstacles{false};
   bool ignore_road_borders{false};
   bool ignore_drivable_area{false};
