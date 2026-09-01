696010c3e78ed2209256093729cbc2d5f3aed01c
Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
2026-09-01 22:01:12 +0900

    fix(trajectory_validator): replace is_feasible boolean to risk level check
    Signed-off-by: Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>

--- planning/autoware_trajectory_validator/include/autoware/trajectory_validator/detail/risk_utils.hpp
+++ planning/autoware_trajectory_validator/include/autoware/trajectory_validator/detail/risk_utils.hpp
@@ -54,5 +54,11 @@ inline RiskLevelType worst_risk_level(const std::vector<MetricReport> & metrics)
   }
   return worst;
 }
+
+inline bool is_feasible_based_on_risk(const RiskLevelType risk_level)
+{
+  return risk_level == RiskLevel::SAFE || risk_level == RiskLevel::LOW_CAUTION ||
+         risk_level == RiskLevel::HIGH_CAUTION;
+}
 }  // namespace autoware::trajectory_validator
 #endif  // AUTOWARE__TRAJECTORY_VALIDATOR__DETAIL__RISK_UTILS_HPP_
