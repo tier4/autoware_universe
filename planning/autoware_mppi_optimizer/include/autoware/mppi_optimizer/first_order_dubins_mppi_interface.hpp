8bf6cdd35e066d6bccfe71988f0978bd2907b621
Maxime CLEMENT <maxime.clement@tier4.jp>
2026-09-08 09:46:57 +0900

    improve physical modeling and jerk calculations
    Signed-off-by: Maxime CLEMENT <maxime.clement@tier4.jp>

--- planning/autoware_mppi_optimizer/include/autoware/mppi_optimizer/first_order_dubins_mppi_interface.hpp
+++ planning/autoware_mppi_optimizer/include/autoware/mppi_optimizer/first_order_dubins_mppi_interface.hpp
@@ -132,6 +132,8 @@ struct FirstOrderDubinsMppiCostBreakdown
   float longitudinal_jerk{0.0F};
   float steering_rate{0.0F};
   float initial_steering_rate{0.0F};
+  float acceleration_command_rate{0.0F};
+  float steering_command_rate{0.0F};
   float kinematic_velocity_overlimit{0.0F};
   float kinematic_acceleration_overlimit{0.0F};
   float kinematic_jerk_overlimit{0.0F};
@@ -148,8 +150,9 @@ struct FirstOrderDubinsMppiCostBreakdown
            lateral_distance + lateral_boundary + lateral_yaw_error + remaining_distance +
            path_overshoot + track_center + corner_buffer + drivable_area + acceleration_command +
            steering_command + lateral_acceleration + lateral_jerk + longitudinal_jerk +
-           steering_rate + initial_steering_rate + kinematic_velocity_overlimit +
-           kinematic_acceleration_overlimit + kinematic_jerk_overlimit + obstacle + road_border;
+           steering_rate + initial_steering_rate + acceleration_command_rate +
+           steering_command_rate + kinematic_velocity_overlimit + kinematic_acceleration_overlimit +
+           kinematic_jerk_overlimit + obstacle + road_border;
   }
 };
 
