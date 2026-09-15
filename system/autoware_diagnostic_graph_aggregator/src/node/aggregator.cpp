shtokuda/autoware.universe

feat(diagnostic_graph_aggregator): apply `agnocast_wrapper::Node` to diagnostic_graph_aggregator (#12843)
* apply agnocast_wrapper::Node

Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

* use AgnocastOnlyCIE

Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

* fix service ptr

Signed-off-by: Koichi Imai <kotty.0704@gmail.com>

* fix

Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

* apply to driving_mode_mapping

Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

* refactor: use plain const reference subscription callbacks

autoware_agnocast_wrapper now accepts rclcpp-style const MessageT &
callbacks (autowarefoundation/autoware_core#1228), so the subscription
callbacks no longer need wrapper-specific message pointer types.

Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>

* style(diagnostic_graph_aggregator): add missing <utility> include for std::move

Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>

---------

Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>
Signed-off-by: Koichi Imai <kotty.0704@gmail.com>
Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>
Co-authored-by: atsushi yano <55824710+atsushi421@users.noreply.github.com>
Co-authored-by: atsushi421 <atsushi.yano.2@tier4.jp>

system/autoware_diagnostic_graph_aggregator/src/node/aggregator.cpp
@@ -20,6 +20,7 @@
 #include <sstream>
 #include <string>
 #include <unordered_map>
+#include <utility>
 
 namespace autoware::diagnostic_graph_aggregator
 {
@@ -82,11 +83,16 @@ AggregatorNode::AggregatorNode(const rclcpp::NodeOptions & options) : Node("aggr
         &AggregatorNode::on_set_override, this, std::placeholders::_1, std::placeholders::_2));
 
     const auto rate = rclcpp::Rate(declare_parameter<double>("rate"));
-    timer_ = rclcpp::create_timer(this, get_clock(), rate.period(), [this]() { on_timer(); });
+    timer_ = autoware::agnocast_wrapper::create_timer(
+      this, get_clock(), rate.period(), [this]() { on_timer(); });
   }
 
   // Send structure topic once.
-  pub_struct_->publish(graph_->create_struct_msg(stamp));
+  {
+    auto struct_msg = ALLOCATE_OUTPUT_MESSAGE_UNIQUE(pub_struct_);
+    *struct_msg = graph_->create_struct_msg(stamp);
+    pub_struct_->publish(std::move(struct_msg));
+  }
 }
 
 AggregatorNode::~AggregatorNode()
@@ -101,8 +107,16 @@ void AggregatorNode::on_timer()
   graph_->update(stamp);
 
   // Publish status.
-  pub_status_->publish(graph_->create_status_msg(stamp));
-  pub_unknown_->publish(graph_->create_unknown_msg(stamp));
+  {
+    auto status_msg = ALLOCATE_OUTPUT_MESSAGE_UNIQUE(pub_status_);
+    *status_msg = graph_->create_status_msg(stamp);
+    pub_status_->publish(std::move(status_msg));
+  }
+  {
+    auto unknown_msg = ALLOCATE_OUTPUT_MESSAGE_UNIQUE(pub_unknown_);
+    *unknown_msg = graph_->create_unknown_msg(stamp);
+    pub_unknown_->publish(std::move(unknown_msg));
+  }
 
   // Update plugins.
   if (command_modes_) command_modes_->update(stamp);
@@ -115,22 +129,24 @@ void AggregatorNode::on_diag(const DiagnosticArray & msg)
 }
 
 void AggregatorNode::on_reset(
-  const ResetDiagGraph::Request::SharedPtr, const ResetDiagGraph::Response::SharedPtr response)
+  AUTOWARE_SERVER_REQUEST_PTR(ResetDiagGraph),
+  AUTOWARE_SERVER_RESPONSE_PTR(ResetDiagGraph) response)
 {
   graph_->reset();
   response->status.success = true;
 }
 
 void AggregatorNode::on_set_initializing(
-  const SetInitializing::Request::SharedPtr request,
-  const SetInitializing::Response::SharedPtr response)
+  AUTOWARE_SERVER_REQUEST_PTR(SetInitializing) request,
+  AUTOWARE_SERVER_RESPONSE_PTR(SetInitializing) response)
 {
   graph_->set_initializing(request->data);
   response->success = true;
 }
 
 void AggregatorNode::on_set_override(
-  const SetOverride::Request::SharedPtr request, const SetOverride::Response::SharedPtr response)
+  AUTOWARE_SERVER_REQUEST_PTR(SetOverride) request,
+  AUTOWARE_SERVER_RESPONSE_PTR(SetOverride) response)
 {
   if (!allow_override_) {
     response->status.success = false;

system/autoware_diagnostic_graph_aggregator/src/node/aggregator.hpp
@@ -19,6 +19,7 @@
 #include "driving_mode_mapping.hpp"
 #include "graph/graph.hpp"
 
+#include <autoware/agnocast_wrapper/node.hpp>
 #include <rclcpp/rclcpp.hpp>
 
 #include <std_srvs/srv/set_bool.hpp>
@@ -32,7 +33,7 @@
 namespace autoware::diagnostic_graph_aggregator
 {
 
-class AggregatorNode : public rclcpp::Node
+class AggregatorNode : public autoware::agnocast_wrapper::Node
 {
 public:
   explicit AggregatorNode(const rclcpp::NodeOptions & options);
@@ -47,25 +48,26 @@ private:
   using ResetDiagGraph = tier4_system_msgs::srv::ResetDiagGraph;
   using SetInitializing = std_srvs::srv::SetBool;
   using SetOverride = tier4_system_msgs::srv::SetDiagGraphOverride;
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Subscription<DiagnosticArray>::SharedPtr sub_input_;
-  rclcpp::Publisher<DiagGraphStruct>::SharedPtr pub_struct_;
-  rclcpp::Publisher<DiagGraphStatus>::SharedPtr pub_status_;
-  rclcpp::Publisher<DiagnosticArray>::SharedPtr pub_unknown_;
-  rclcpp::Service<ResetDiagGraph>::SharedPtr srv_reset_;
-  rclcpp::Service<SetInitializing>::SharedPtr srv_set_initializing_;
-  rclcpp::Service<SetOverride>::SharedPtr srv_set_override_;
+  AUTOWARE_TIMER_PTR timer_;
+  AUTOWARE_SUBSCRIPTION_PTR(DiagnosticArray) sub_input_;
+  AUTOWARE_PUBLISHER_PTR(DiagGraphStruct) pub_struct_;
+  AUTOWARE_PUBLISHER_PTR(DiagGraphStatus) pub_status_;
+  AUTOWARE_PUBLISHER_PTR(DiagnosticArray) pub_unknown_;
+  AUTOWARE_SERVICE_PTR(ResetDiagGraph) srv_reset_;
+  AUTOWARE_SERVICE_PTR(SetInitializing) srv_set_initializing_;
+  AUTOWARE_SERVICE_PTR(SetOverride) srv_set_override_;
 
   void on_timer();
   void on_diag(const DiagnosticArray & msg);
   void on_reset(
-    const ResetDiagGraph::Request::SharedPtr request,
-    const ResetDiagGraph::Response::SharedPtr response);
+    AUTOWARE_SERVER_REQUEST_PTR(ResetDiagGraph) request,
+    AUTOWARE_SERVER_RESPONSE_PTR(ResetDiagGraph) response);
   void on_set_initializing(
-    const SetInitializing::Request::SharedPtr request,
-    const SetInitializing::Response::SharedPtr response);
+    AUTOWARE_SERVER_REQUEST_PTR(SetInitializing) request,
+    AUTOWARE_SERVER_RESPONSE_PTR(SetInitializing) response);
   void on_set_override(
-    const SetOverride::Request::SharedPtr request, const SetOverride::Response::SharedPtr response);
+    AUTOWARE_SERVER_REQUEST_PTR(SetOverride) request,
+    AUTOWARE_SERVER_RESPONSE_PTR(SetOverride) response);
 };
 
 }  // namespace autoware::diagnostic_graph_aggregator

tier4/autoware_universe

(cherry picked from commit c98afcab08456f74a81ffcf8fdb82b10290538ee)
Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

system/autoware_diagnostic_graph_aggregator/src/node/aggregator.cpp
@@ -20,6 +20,7 @@
 #include <sstream>
 #include <string>
 #include <unordered_map>
+#include <utility>
 
 namespace autoware::diagnostic_graph_aggregator
 {
@@ -72,11 +73,16 @@ AggregatorNode::AggregatorNode(const rclcpp::NodeOptions & options) : Node("aggr
         &AggregatorNode::on_set_initializing, this, std::placeholders::_1, std::placeholders::_2));
 
     const auto rate = rclcpp::Rate(declare_parameter<double>("rate"));
-    timer_ = rclcpp::create_timer(this, get_clock(), rate.period(), [this]() { on_timer(); });
+    timer_ = autoware::agnocast_wrapper::create_timer(
+      this, get_clock(), rate.period(), [this]() { on_timer(); });
   }
 
   // Send structure topic once.
-  pub_struct_->publish(graph_->create_struct_msg(stamp));
+  {
+    auto struct_msg = ALLOCATE_OUTPUT_MESSAGE_UNIQUE(pub_struct_);
+    *struct_msg = graph_->create_struct_msg(stamp);
+    pub_struct_->publish(std::move(struct_msg));
+  }
 }
 
 AggregatorNode::~AggregatorNode()
@@ -91,8 +97,16 @@ void AggregatorNode::on_timer()
   graph_->update(stamp);
 
   // Publish status.
-  pub_status_->publish(graph_->create_status_msg(stamp));
-  pub_unknown_->publish(graph_->create_unknown_msg(stamp));
+  {
+    auto status_msg = ALLOCATE_OUTPUT_MESSAGE_UNIQUE(pub_status_);
+    *status_msg = graph_->create_status_msg(stamp);
+    pub_status_->publish(std::move(status_msg));
+  }
+  {
+    auto unknown_msg = ALLOCATE_OUTPUT_MESSAGE_UNIQUE(pub_unknown_);
+    *unknown_msg = graph_->create_unknown_msg(stamp);
+    pub_unknown_->publish(std::move(unknown_msg));
+  }
 
   // Update plugins.
   if (availability_) availability_->update(stamp);
@@ -104,14 +118,16 @@ void AggregatorNode::on_diag(const DiagnosticArray & msg)
 }
 
 void AggregatorNode::on_reset(
-  const ResetDiagGraph::Request::SharedPtr, const ResetDiagGraph::Response::SharedPtr response)
+  AUTOWARE_SERVER_REQUEST_PTR(ResetDiagGraph),
+  AUTOWARE_SERVER_RESPONSE_PTR(ResetDiagGraph) response)
 {
   graph_->reset();
   response->status.success = true;
 }
 
 void AggregatorNode::on_set_initializing(
-  const SetBool::Request::SharedPtr request, const SetBool::Response::SharedPtr response)
+  AUTOWARE_SERVER_REQUEST_PTR(SetBool) request,
+  AUTOWARE_SERVER_RESPONSE_PTR(SetBool) response)
 {
   graph_->set_initializing(request->data);
   response->success = true;

system/autoware_diagnostic_graph_aggregator/src/node/aggregator.hpp
@@ -18,6 +18,7 @@
 #include "command_mode_mapping.hpp"
 #include "graph/graph.hpp"
 
+#include <autoware/agnocast_wrapper/node.hpp>
 #include <rclcpp/rclcpp.hpp>
 
 #include <std_srvs/srv/set_bool.hpp>
@@ -29,7 +30,7 @@
 namespace autoware::diagnostic_graph_aggregator
 {
 
-class AggregatorNode : public rclcpp::Node
+class AggregatorNode : public autoware::agnocast_wrapper::Node
 {
 public:
   explicit AggregatorNode(const rclcpp::NodeOptions & options);
@@ -40,21 +41,22 @@ private:
   std::unique_ptr<CommandModeMapping> availability_;
 
   using SetBool = std_srvs::srv::SetBool;
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Subscription<DiagnosticArray>::SharedPtr sub_input_;
-  rclcpp::Publisher<DiagGraphStruct>::SharedPtr pub_struct_;
-  rclcpp::Publisher<DiagGraphStatus>::SharedPtr pub_status_;
-  rclcpp::Publisher<DiagnosticArray>::SharedPtr pub_unknown_;
-  rclcpp::Service<ResetDiagGraph>::SharedPtr srv_reset_;
-  rclcpp::Service<SetBool>::SharedPtr srv_set_initializing_;
+  AUTOWARE_TIMER_PTR timer_;
+  AUTOWARE_SUBSCRIPTION_PTR(DiagnosticArray) sub_input_;
+  AUTOWARE_PUBLISHER_PTR(DiagGraphStruct) pub_struct_;
+  AUTOWARE_PUBLISHER_PTR(DiagGraphStatus) pub_status_;
+  AUTOWARE_PUBLISHER_PTR(DiagnosticArray) pub_unknown_;
+  AUTOWARE_SERVICE_PTR(ResetDiagGraph) srv_reset_;
+  AUTOWARE_SERVICE_PTR(SetBool) srv_set_initializing_;
 
   void on_timer();
   void on_diag(const DiagnosticArray & msg);
   void on_reset(
-    const ResetDiagGraph::Request::SharedPtr request,
-    const ResetDiagGraph::Response::SharedPtr response);
+    AUTOWARE_SERVER_REQUEST_PTR(ResetDiagGraph) request,
+    AUTOWARE_SERVER_RESPONSE_PTR(ResetDiagGraph) response);
   void on_set_initializing(
-    const SetBool::Request::SharedPtr request, const SetBool::Response::SharedPtr response);
+    AUTOWARE_SERVER_REQUEST_PTR(SetBool) request,
+    AUTOWARE_SERVER_RESPONSE_PTR(SetBool) response);
 };
 
 }  // namespace autoware::diagnostic_graph_aggregator